95ca47aed0a737b609ac801ad1b2a84c354a1a76
いぬふと <mxb03523@nifty.com>
2024-09-22 08:51:37 +0900

    Add MZ-80B Yewdow

---
+++ mz80b/yewdow/Status.c
@@ -0,0 +1,150 @@
+#include "Status.h"
+#include "Vram.h"
+#include "Stage.h"
+#include "Main.h"
+#include "Sprite.h"
+#include "Print.h"
+#include "Chars.h"
+
+constexpr byte Char_Remain = Char_Car;
+
+static word PrintS(word vram, ptr<byte> p)
+{
+    byte c;
+    while ((c = *p) != 0) {
+        vram = PrintC(vram, c);
+        ++p;
+    }
+    return vram;
+}
+
+void PrintStatus() 
+{
+    PrintS(Vram + VramRowSize * 1 + 32 * VramStep, "SCORE");
+    PrintS(Vram + VramRowSize * 5 + 32 * VramStep, "HI-SCORE");
+    PrintS(Vram + VramRowSize * 9 + 32 * VramStep, "STAGE");
+    PrintByteNumber2(Vram + VramRowSize * 9 + 38 * VramStep, CurrentStage + 1);
+    PrintS(Vram + VramRowSize * 12 + 32 * VramStep, "TIME");
+
+    {
+        word vram;
+        vram = Vram + VramRowSize * 21 + 33 * VramStep;
+        if (RemainCount > 1) {
+            byte i;
+            i = RemainCount - 1;
+            if (i > 2) {
+                vram = Put2C(vram, Char_Remain);
+                Erase2(vram);
+                vram = Put(Vram + VramRowSize, i + 0x10);
+            }
+            else {
+                do {
+                    vram = Put2C(vram, Char_Remain);
+                    --i;
+                } while (i > 0);
+            }
+        }
+    }
+    PrintScore();
+    PrintTime();
+}
+
+void PrintScore()
+{
+    word vram;
+    
+    vram = PrintNumber5(Vram + VramRowSize * 2 + 34 * VramStep, Score);
+    PrintC(vram, '0');
+
+    vram = PrintNumber5(Vram + VramRowSize * 6 + 34 * VramStep, HiScore);
+    PrintC(vram, '0');
+}
+
+void PrintTime()
+{
+    PrintByteNumber3(Vram + VramRowSize * 12 + 37 * VramStep, StageTime);
+}
+
+void PrintGameOver()
+{
+    PrintS(Vram + VramRowSize * 11 + 12 * VramStep, "GAME OVER");
+}
+
+void PrintTimeUp() 
+{
+    PrintS(Vram + VramRowSize * 11 + 13 * VramStep, "TIME UP");
+}
+
+
+void Title()
+{
+    ClearScreen(); 
+    HideAllSprites();
+    PrintStatus();
+    {
+        static const byte[] TitleBytes = {
+            0x4f, 0x40, 0x4f, 0x40, 
+            0x4d, 0x4a, 0x47, 0x48, 
+            0x40, 0x4f, 0x40, 0x4c, 
+            0x40, 0x45, 0x40, 0x40, 
+            0x40, 0x40, 0x40, 0x40, 
+            0x47, 0x4b, 0x4c, 0x43, 
+            0x47, 0x45, 0x4c, 0x43, 
+            0x45, 0x41, 0x44, 0x45, 
+            0x40, 0x40, 0x40, 0x40, 
+            0x43, 0x4f, 0x48, 0x47, 
+            0x43, 0x4f, 0x4c, 0x43, 
+            0x45, 0x41, 0x40, 0x45, 
+            0x4f, 0x40, 0x40, 0x40, 
+            0x4f, 0x48, 0x47, 0x4b, 
+            0x4f, 0x4c, 0x43, 0x4f, 
+            0x45, 0x40, 0x45, 0x41, 
+            0x40, 0x40, 0x40, 0x40, 
+            0x4c, 0x43, 0x43, 0x4f, 
+            0x4c, 0x43, 0x43, 0x4f, 
+            0x44, 0x45, 0x45, 0x41, 
+        };
+        ptr<byte> p;
+        word vram;
+        vram = Vram + VramRowSize * 8 + 6 * VramStep;
+        p = TitleBytes;
+        repeat (5) {
+            repeat (4) {
+                repeat (4) {
+                    vram = Put(vram, *p);
+                    ++p;
+                }
+                vram += VramRowSize - 4 * VramStep;
+            }
+            vram += 4 * VramStep - VramRowSize * 4;
+        }
+    }
+    PrintS(Vram + VramRowSize * 20 + 9 * VramStep, "PUSH SPACE KEY");
+    PrintS(Vram + VramRowSize * 21 + 9 * VramStep, "OR TAB KEY");
+    PrintS(Vram + VramRowSize * 23 + 20 * VramStep, "INUFUTO 2023");
+    // {
+    //     word vram;
+    //     byte c;
+    //     vram = Vram;
+    //     c = 0;
+    //     repeat (0) {
+    //         vram = Put(vram, c);
+    //         ++c;
+    //         if ((c & 0x0f) == 0) {
+    //             vram += VramRowSize - 16 * VramStep;
+    //         }
+    //     }
+    // }
+}
+
+
+void DrawFence()
+{
+    word top, bottom;
+    top = Vram;
+    bottom = Vram + VramRowSize * 23;
+    repeat(StageWidth) {
+        top = Put(top, Char_Fence);
+        bottom = Put(bottom, Char_Fence + 1);
+    }
+}
