a4917c176e1fb45a3dbc50518d1c813e4f22265d
いぬふと <mxb03523@nifty.com>
2024-09-23 08:12:20 +0900

    Add MZ-2000 AntiAir

---
+++ mz2000mono/antiair/Chars.asm
@@ -0,0 +1,141 @@
+cseg
+
+CharPattern: public CharPattern
+; Ascii
+	defb 000H,000H,000H,000H,000H,000H,000H,000H
+	defb 018H,018H,018H,018H,018H,000H,018H,000H
+	defb 036H,036H,036H,000H,000H,000H,000H,000H
+	defb 006H,037H,03eH,077H,03eH,076H,030H,000H
+	defb 008H,03fH,048H,03eH,009H,07eH,008H,000H
+	defb 000H,032H,054H,068H,016H,02aH,04cH,000H
+	defb 038H,06cH,06cH,038H,06bH,066H,03bH,000H
+	defb 00cH,018H,030H,000H,000H,000H,000H,000H
+	defb 00cH,018H,030H,030H,030H,018H,00cH,000H
+	defb 030H,018H,00cH,00cH,00cH,018H,030H,000H
+	defb 008H,02aH,01cH,07fH,01cH,02aH,008H,000H
+	defb 000H,018H,018H,07eH,018H,018H,000H,000H
+	defb 000H,000H,000H,000H,018H,018H,030H,000H
+	defb 000H,000H,000H,07eH,000H,000H,000H,000H
+	defb 000H,000H,000H,000H,000H,018H,018H,000H
+	defb 000H,003H,006H,00cH,018H,030H,060H,000H
+	defb 03eH,063H,067H,06bH,073H,063H,03eH,000H
+	defb 018H,078H,018H,018H,018H,018H,07eH,000H
+	defb 03eH,063H,007H,01eH,03cH,070H,07fH,000H
+	defb 03eH,063H,003H,01eH,003H,063H,03eH,000H
+	defb 006H,00eH,01eH,036H,066H,07fH,006H,000H
+	defb 07fH,060H,07eH,003H,003H,063H,03eH,000H
+	defb 01eH,030H,060H,07eH,063H,063H,03eH,000H
+	defb 07fH,063H,006H,00cH,018H,018H,018H,000H
+	defb 03eH,063H,063H,03eH,063H,063H,03eH,000H
+	defb 03eH,063H,063H,03fH,003H,063H,03eH,000H
+	defb 000H,000H,018H,000H,000H,018H,000H,000H
+	defb 000H,000H,018H,000H,000H,018H,030H,000H
+	defb 00cH,018H,030H,060H,030H,018H,00cH,000H
+	defb 000H,000H,07eH,000H,07eH,000H,000H,000H
+	defb 030H,018H,00cH,006H,00cH,018H,030H,000H
+	defb 03eH,063H,003H,00eH,018H,000H,018H,000H
+	defb 01eH,021H,04dH,055H,04eH,020H,01eH,000H
+	defb 01cH,036H,063H,063H,07fH,063H,063H,000H
+	defb 07eH,063H,063H,07eH,063H,063H,07eH,000H
+	defb 01eH,033H,060H,060H,060H,033H,01eH,000H
+	defb 07cH,066H,063H,063H,063H,066H,07cH,000H
+	defb 07eH,060H,060H,07eH,060H,060H,07eH,000H
+	defb 07eH,060H,060H,07eH,060H,060H,060H,000H
+	defb 01eH,033H,060H,06fH,063H,033H,01fH,000H
+	defb 063H,063H,063H,07fH,063H,063H,063H,000H
+	defb 07eH,018H,018H,018H,018H,018H,07eH,000H
+	defb 006H,006H,006H,006H,006H,066H,03cH,000H
+	defb 063H,066H,06cH,078H,06cH,066H,063H,000H
+	defb 060H,060H,060H,060H,060H,060H,07eH,000H
+	defb 07eH,06bH,06bH,06bH,06bH,06bH,06bH,000H
+	defb 063H,073H,07bH,07fH,06fH,067H,063H,000H
+	defb 03eH,063H,063H,063H,063H,063H,03eH,000H
+	defb 07eH,063H,063H,063H,07eH,060H,060H,000H
+	defb 03eH,063H,063H,063H,06bH,066H,03bH,000H
+	defb 07eH,063H,063H,067H,07cH,06eH,067H,000H
+	defb 03eH,063H,060H,03eH,003H,063H,03eH,000H
+	defb 07eH,018H,018H,018H,018H,018H,018H,000H
+	defb 063H,063H,063H,063H,063H,063H,03eH,000H
+	defb 063H,063H,063H,036H,036H,01cH,01cH,000H
+	defb 063H,06bH,06bH,06bH,06bH,06bH,07eH,000H
+	defb 063H,063H,036H,01cH,036H,063H,063H,000H
+	defb 066H,066H,066H,03cH,018H,018H,018H,000H
+	defb 07fH,006H,00cH,018H,030H,060H,07fH,000H
+	defb 03cH,020H,020H,020H,020H,020H,03cH,000H
+	defb 022H,022H,014H,03eH,008H,03eH,008H,000H
+	defb 03cH,004H,004H,004H,004H,004H,03cH,000H
+	defb 008H,014H,022H,000H,000H,000H,000H,000H
+	defb	7ch, 82h, 0bah, 0a2h, 0bah, 82h, 7ch, 00h
+; Logo
+	defb	00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h
+	defb	0f0h, 0f0h, 0f0h, 0f0h, 00h, 00h, 00h, 00h
+	defb	00h, 00h, 00h, 00h, 0f0h, 0f0h, 0f0h, 0f0h
+	defb	0f0h, 0f0h, 0f0h, 0f0h, 0f0h, 0f0h, 0f0h, 0f0h
+	defb	0fh, 0fh, 0fh, 0fh, 00h, 00h, 00h, 00h
+	defb	0ffh, 0ffh, 0ffh, 0ffh, 00h, 00h, 00h, 00h
+	defb	0fh, 0fh, 0fh, 0fh, 0f0h, 0f0h, 0f0h, 0f0h
+	defb	0ffh, 0ffh, 0ffh, 0ffh, 0f0h, 0f0h, 0f0h, 0f0h
+	defb	00h, 00h, 00h, 00h, 0fh, 0fh, 0fh, 0fh
+	defb	0f0h, 0f0h, 0f0h, 0f0h, 0fh, 0fh, 0fh, 0fh
+	defb	00h, 00h, 00h, 00h, 0ffh, 0ffh, 0ffh, 0ffh
+	defb	0f0h, 0f0h, 0f0h, 0f0h, 0ffh, 0ffh, 0ffh, 0ffh
+	defb	0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh
+	defb	0ffh, 0ffh, 0ffh, 0ffh, 0fh, 0fh, 0fh, 0fh
+	defb	0fh, 0fh, 0fh, 0fh, 0ffh, 0ffh, 0ffh, 0ffh
+	defb	0ffh, 0ffh, 0ffh, 0ffh, 0ffh, 0ffh, 0ffh, 0ffh
+;	8dot
+	defb	0ffh, 00h, 00h, 00h, 00h, 00h, 00h, 00h
+;	16dot
+	defb	01h, 03h, 03h, 07h, 07h, 0fh, 0fh, 0fh
+	defb	00h, 80h, 80h, 0c0h, 0c0h, 0e0h, 0e0h, 0e0h
+	defb	2eh, 6dh, 0edh, 0edh, 0edh, 00h, 0ffh, 7ch
+	defb	0e8h, 6ch, 6eh, 6eh, 6eh, 00h, 0feh, 7ch
+	defb	00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h
+	defb	00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h
+	defb	00h, 00h, 00h, 00h, 00h, 00h, 0ffh, 7ch
+	defb	00h, 00h, 00h, 00h, 00h, 00h, 0feh, 7ch
+	defb	01h, 03h, 03h, 07h, 07h, 0fh, 0fh, 0fh
+	defb	00h, 80h, 80h, 0c0h, 0c0h, 0e0h, 0e0h, 0e0h
+	defb	2eh, 6dh, 0edh, 0edh, 0edh, 00h, 07h, 03h
+	defb	0e8h, 6ch, 6eh, 6eh, 6eh, 00h, 0c0h, 80h
+	defb	00h, 7fh, 0c0h, 0bfh, 0bfh, 0bfh, 0bfh, 0bfh
+	defb	00h, 0fch, 06h, 0fah, 0fah, 0fah, 0fah, 0fah
+	defb	0bfh, 0bfh, 0bfh, 0bfh, 0bfh, 0bfh, 0c0h, 7fh
+	defb	0fah, 0fah, 0fah, 0fah, 0fah, 0fah, 06h, 0fch
+	defb	00h, 0e1h, 0f5h, 75h, 2eh, 1fh, 1fh, 3fh
+	defb	00h, 0eh, 5eh, 5ch, 0e8h, 0f0h, 0f0h, 0f8h
+	defb	3fh, 2ah, 3fh, 3fh, 1fh, 1fh, 0fh, 03h
+	defb	0f8h, 0a8h, 0f8h, 0f8h, 0f0h, 0f0h, 0e0h, 80h
+	defb	00h, 01h, 02h, 02h, 07h, 0fh, 1fh, 3fh
+	defb	00h, 0c0h, 34h, 02h, 08h, 80h, 40h, 0a0h
+	defb	7fh, 7fh, 7fh, 7fh, 7fh, 3fh, 1fh, 0fh
+	defb	0f0h, 0f0h, 0f0h, 0f0h, 0f0h, 0e0h, 0c0h, 80h
+	defb	01h, 21h, 33h, 3eh, 24h, 0e1h, 77h, 3bh
+	defb	04h, 9ch, 0f8h, 0f9h, 6fh, 0eh, 9ch, 0b8h
+	defb	1bh, 30h, 0e2h, 6eh, 3eh, 1bh, 11h, 01h
+	defb	0dch, 0cfh, 0eh, 48h, 0ech, 0fch, 8ch, 00h
+	defb	00h, 00h, 08h, 0eh, 07h, 07h, 06h, 03h
+	defb	02h, 07h, 07h, 0fh, 9dh, 0fch, 78h, 38h
+	defb	03h, 1fh, 0f3h, 78h, 3eh, 1fh, 0fh, 06h
+	defb	12h, 82h, 9fh, 3bh, 79h, 19h, 98h, 30h
+	defb	00h, 00h, 01h, 87h, 8fh, 0dfh, 0fdh, 79h
+	defb	60h, 0e0h, 0c0h, 0c0h, 0c0h, 80h, 8eh, 0bch
+	defb	71h, 25h, 0ch, 9ch, 0f8h, 19h, 11h, 38h
+	defb	0f8h, 0f8h, 30h, 0e0h, 0c0h, 0c0h, 0e0h, 0f0h
+	defb	04h, 09h, 39h, 73h, 0e0h, 7fh, 3fh, 0eh
+	defb	64h, 0ceh, 9eh, 0ffh, 7dh, 21h, 6dh, 1ch
+	defb	06h, 02h, 03h, 03h, 02h, 00h, 00h, 00h
+	defb	7eh, 0eeh, 0ceh, 07h, 07h, 03h, 02h, 02h
+	defb	7fh, 60h, 0f0h, 0f3h, 9bh, 09h, 21h, 38h
+	defb	38h, 0ch, 0feh, 0ffh, 0f0h, 80h, 80h, 0c0h
+	defb	7ch, 0ffh, 0e3h, 0c1h, 80h, 00h, 00h, 00h
+	defb	0c0h, 0c0h, 0e0h, 0e0h, 60h, 00h, 00h, 00h
+;	ufo
+	defb	00h, 01h, 07h, 0fh, 1fh, 0fh, 01h, 06h
+	defb	3fh, 0ffh, 0fdh, 0fdh, 0fdh, 0ffh, 0ffh, 00h
+	defb	0f8h, 0ffh, 7fh, 7fh, 7fh, 0ffh, 0ffh, 00h
+	defb	00h, 00h, 0c0h, 0e0h, 0f0h, 0e0h, 00h, 0c0h
+	defb	0eh, 0ch, 06h, 06h, 02h, 00h, 00h, 00h
+	defb	00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h
+	defb	00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h
+	defb	0e0h, 60h, 0c0h, 0c0h, 80h, 00h, 00h, 00h
---
+++ mz2000mono/antiair/Status.h
@@ -0,0 +1,12 @@
+extern word Score;
+extern word HiScore;
+extern byte RemainCount;
+extern byte CurrentStage;
+
+extern void PrintStatus();
+extern void PrintScore();
+extern void PrintStage();
+extern void PrintGameOver();
+extern void Title();
+extern void AddScore(word pts);
+extern void DrawGround();
