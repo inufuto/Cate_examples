12049236d0a94e89cd8b2cd0ac481dc8237f98f4
いぬふと <mxb03523@nifty.com>
2024-09-22 14:50:15 +0900

    Add MZ-2200 IMPETUS

---
+++ mz2000color/impetus/Main.c
@@ -0,0 +1,117 @@
+#include "Main.h"
+// #include "Init.h"
+#include "Stage.h"
+#include "ScanKeys.h"
+#include "VVram.h"
+#include "Sprite.h"
+#include "Fighter.h"
+#include "FighterBullet.h"
+#include "SkyEnemy.h"
+#include "EnemyBullet.h"
+#include "Status.h"
+#include "Bang.h"
+#include "Barrier.h"
+#include "GroundEnemy.h"
+#include "Fort.h"
+#include "Sound.h"
+#include "Item.h"
+#include "Vram.h"
+
+extern void WaitTimer(byte t);
+
+word Score;
+word HiScore;
+byte RemainCount;
+byte CurrentStage;
+static byte Clock;
+
+void Main()
+{
+    byte key;
+    
+    // Init();
+    InitSprites();
+    HiScore = 0;
+    Score = 0;
+    CurrentStage = 0;
+    RemainCount = 3;
+
+    title:
+    Title();
+    do {
+        // if (ScanStop()) goto exit;
+        key = ScanKeys();
+    } while ((key & (Keys_Button0 | Keys_Button1)) == 0);
+
+play:
+    Score = 0;
+    if ((key & Keys_Button1) == 0) {
+        CurrentStage = 0;
+                            // CurrentStage = 4;
+    }
+    RemainCount = 3;
+    InitStage();
+    InitTrying();
+    PrintStatus();
+    DrawAll();
+    VVramToVramAll();
+    Sound_Start();
+    // StartBGM();
+    Clock = 0;
+loop:
+    // if (ScanStop()) goto exit;
+    if ((Clock & 0x01) == 0) {
+        MoveFighter();
+        MoveEnemyBullets();
+    }
+    //PollVSync();
+    if ((Clock & 0x03) == 0) {
+        MoveSkyEnemies();
+        MoveBarriers();
+        MoveItem();
+    }
+    //PollVSync();
+    if ((Clock & 0x07) == 0) {
+        MoveFort();
+    }
+    //PollVSync();
+    MoveFighterBullets();
+    if ((Clock & 0x0f) == 0) {
+        ScrollGround();
+        MoveGroundEnemy();
+    }
+    //PollVSync();
+
+    if ((Clock & 0x01) == 0) {
+        UpdateBangs();
+        WaitTimer(4);
+        DrawAll();
+        CallSound();
+    }
+
+    if (RemainCount == 0) {
+        // StopBGM();
+        PrintGameOver();
+        Sound_GameOver();
+        goto title;
+    }
+
+    ++Clock;
+    goto loop;
+// exit:
+//     Fin();
+}
+
+
+void AddScore(word pts)
+{
+    Score += pts;
+    if (Score > HiScore) {
+        HiScore = Score;
+    }
+    PrintScore();
+    // StatusToVVram();
+}
+
+
+void _deb(){}
---
+++ mz2000color/impetus/Vram.c
@@ -0,0 +1,24 @@
+#include "Vram.h"
+#include "Chars.h"
+
+extern byte[] Backup;
+extern byte[] MonoPattern, ColorPattern, AsciiPattern;
+
+
+void InitVram()
+{
+    ClearScreen();
+    MakePatternMono(Char_Ascii, AsciiPattern, 0x40, 7);
+    MakePatternMono(Char_Logo, MonoPattern + (Char_Logo - Char_Logo) * CharHeight, 16, 5);
+    MakePatternMono(
+        Char_FighterBullet, MonoPattern + (Char_FighterBullet - Char_Logo) * CharHeight, 3, 6
+    );
+    MakePatternColor(Char_BarrierHead, ColorPattern, Char_End - Char_BarrierHead);
+}
+
+
+word PrintC(word vram, byte c)
+{
+    VPut(vram, c - ' ');
+    return vram + 2;
+}
