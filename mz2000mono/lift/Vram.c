146d26e6defe44f50ed99f9c9dcf8b4ab4f84d83
いぬふと <mxb03523@nifty.com>
2024-09-23 09:37:02 +0900

    Add MZ-2000 LIFT

---
+++ mz2000mono/lift/Vram.c
@@ -0,0 +1,7 @@
+#include "Vram.h"
+
+word PrintC(word vram, byte c)
+{
+    VPut(vram, c - ' ');
+    return vram + 2;
+}
---
+++ mz2000mono/lift/Vram.h
@@ -0,0 +1,14 @@
+constexpr word Vram = 0xc000;
+constexpr byte VramWidth = 80;
+constexpr byte VramHeight = 25;
+constexpr byte CharHeight = 8;
+constexpr byte VramRowSize = VramWidth * CharHeight;
+
+extern void MakePatternMono(byte c, ptr<byte> pSource, byte count, byte color);
+extern void MakePatternColor(byte c, ptr<byte> pSource, byte count);
+extern void ClearScreen();
+extern void VPut(word vram, byte c);
+extern void VVramToVram();
+
+extern void InitVram();
+extern word PrintC(word vram, byte c);
