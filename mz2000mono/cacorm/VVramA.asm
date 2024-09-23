80eec24d5ca8cd492fbbb1a9076856b52cc809ef
いぬふと <mxb03523@nifty.com>
2024-09-23 11:43:58 +0900

    Add MZ-2000 Cacorm

---
+++ mz2000mono/cacorm/VVramA.asm
@@ -0,0 +1,148 @@
+include "VVram.inc"
+include "Chars.inc"
+
+ext VVram_
+ext CellMap_
+ext LineRed_
+
+MapWidth equ VVramWidth/2
+MapHeight equ VVramHeight/2
+
+Cell_Wall equ 10h
+Cell_Item equ 20h
+Cell_Mask equ 0f0h
+
+cseg
+
+; ptr<byte> VVramPtr(byte x, byte y);
+VVramPtr_: public VVramPtr_
+    push de
+        ex de,hl
+            ld h,0
+            add hl,hl ;*2
+            add hl,hl ;*4
+            add hl,hl ;*8
+            add hl,hl ;*16
+            add hl,hl ;*32
+        ex de,hl
+        ld l,a
+        ld h,0
+        add hl,de
+        ld de,VVram_
+        add hl,de
+    pop de
+ret
+
+
+; void MapToVVram();
+cseg
+LineChars:
+    defb Char_Space, Char_Space, Char_Space, Char_Space ; none
+    defb Char_Line_Bottom, Char_Space, Char_Line_Top, Char_Space ; left
+    defb Char_Space, Char_Line_Bottom, Char_Space, Char_Line_Top ; right
+    defb Char_Line_Bottom, Char_Line_Bottom, Char_Line_Top, Char_Line_Top ; left right
+    defb Char_Line_Right, Char_Line_Left, Char_Space, Char_Space ; top
+    defb Char_Line_RightBottom, Char_Line_Left, Char_Line_Top, Char_Space ; top left
+    defb Char_Line_Right, Char_Line_LeftBottom, Char_Space, Char_Line_Top ; top right
+    defb Char_Line_RightBottom, Char_Line_LeftBottom, Char_Line_Top, Char_Line_Top ; top left right
+    defb Char_Space, Char_Space, Char_Line_Right, Char_Line_Left ; bottom
+    defb Char_Line_Bottom, Char_Space, Char_Line_RightTop, Char_Line_Left ; bottom left
+    defb Char_Space, Char_Line_Bottom, Char_Line_Right, Char_Line_LeftTop ; bottom right
+    defb Char_Line_Bottom, Char_Line_Bottom, Char_Line_RightTop, Char_Line_LeftTop ; bottom left right
+    defb Char_Line_Right, Char_Line_Left, Char_Line_Right, Char_Line_Left ; bottom top
+    defb Char_Line_RightBottom, Char_Line_Left, Char_Line_RightTop, Char_Line_Left ; bottom top left
+    defb Char_Line_Right, Char_Line_LeftBottom, Char_Line_Right, Char_Line_LeftTop ; bottom top right
+    defb Char_Line_RightBottom, Char_Line_LeftBottom, Char_Line_RightTop, Char_Line_LeftTop ; bottom top left right
+
+MapToVVram_: public MapToVVram_
+    push af | push hl | push de | push bc
+        ld hl,VVram_
+        ld de,CellMap_
+        ld c,MapHeight
+        do
+            ld b,MapWidth
+            do
+                push bc
+                    ld a,(de) | inc de
+                    cp Cell_Wall
+                    if z
+                        ld a,Char_Block
+                        jr byChar
+                    endif
+                    cp Cell_Item
+                    if z
+                        ld a,Char_Item
+                        byChar:
+                        push bc
+                            ld c,2
+                            do
+                                ld b,2
+                                do
+                                    ld (hl),a | inc hl
+                                    inc a
+                                dwnz
+                                push de
+                                    ld de,VVramWidth-2
+                                    add hl,de
+                                pop de
+                                dec c
+                            while nz | wend
+                        pop bc
+                        jr nextCell
+                    endif
+                    and 0fh
+                    add a,a
+                    add a,a
+                    push de
+                        add a,low LineChars
+                        ld e,a
+                        ld a,high LineChars
+                        adc a,0
+                        ld d,a
+                        push bc
+                            ld c,2
+                            do
+                                ld b,2
+                                do
+                                    ld a,(de) | inc de
+                                    push af
+                                        or a
+                                        if nz
+                                            push de
+                                                ld e,a
+                                                ld a,(LineRed_)
+                                                or a
+                                                if nz
+                                                    ld a,e
+                                                    add a,Char_Line_Bold-Char_Line_Normal
+                                                    ld e,a
+                                                endif
+                                                ld a,e
+                                            pop de
+                                        endif
+                                        ld (hl),a | inc hl
+                                    pop af
+                                dwnz
+                                push de
+                                    ld de,VVramWidth-2
+                                    add hl,de
+                                pop de
+                                dec c
+                            while nz | wend
+                        pop bc
+                    pop de
+                    nextCell:
+                    push de
+                        ld de,2-VVramWidth*2
+                        add hl,de
+                    pop de
+                pop bc
+            dwnz
+            push de
+                ld de,VVramWidth
+                add hl,de
+            pop de
+            dec c
+        while nz | wend
+    pop bc | pop de | pop hl | pop af
+ret
---
+++ mz2000mono/cacorm/VramA.asm
@@ -0,0 +1,190 @@
+include "mz2000.inc"
+include "Chars.inc"
+
+ext CharPattern
+ext VVram_
+
+Vram equ 0c000h
+
+VramWidth equ 80
+VramHeight equ 25
+CharHeight equ 8
+VramRowSize equ VramWidth*CharHeight
+VramTop equ Vram+VramRowSize
+
+PatternSize equ CharHeight*2
+PatternRam equ 8000h
+
+VVramWidth equ 32
+VVramHeight equ 22
+
+dseg
+Backup:
+    defs VVramWidth*VVramHeight
+
+cseg
+PatternAddress:
+    push de
+        ld l,a
+        ld h,0
+        add hl,hl ;*2
+        add hl,hl ;*4
+        add hl,hl ;*8
+        add hl,hl ;*16
+        ld de,PatternRam
+        add hl,de
+    pop de
+ret
+
+
+cseg
+InitVram: public InitVram
+    ld hl,CharPattern
+    exx
+        ld hl,PatternRam
+    exx
+    ld c,Char_End
+    do
+        ld b,CharHeight
+        do
+            ld a,(hl) | inc hl
+            exx
+                ld de,0
+                ld b,8
+                do
+                    srl d | rr e
+                    srl d | rr e
+                    rlca
+                    if c
+                        set 7,d | set 6,d
+                    endif
+                dwnz
+                ld (hl),e | inc hl
+                ld (hl),d | inc hl
+            exx
+        dwnz
+        dec c
+    while nz | wend
+ret
+
+
+; void ClearScreen();
+cseg
+ClearScreen_:   public ClearScreen_
+    push af | push hl | push de | push bc
+        di
+            ld a,1
+            do
+                out (0f7h),a
+                ld hl,0c000h
+                ld de,0c001h
+                ld bc,640/8*200-1
+                ld (hl),0
+                ldir
+                inc a
+                cp 4
+            while nz | wend
+            xor a
+            out (0f7h),a
+        ei
+
+        ld hl,Backup
+        ld bc,VVramWidth * VVramHeight
+        do
+            ld (hl),0
+            inc hl
+            dec bc
+            ld a,c
+            or b
+        while nz | wend
+    pop bc | pop de | pop hl | pop af
+ret
+
+
+; void word Put(word vram, byte c);
+cseg
+Put_: public Put_
+    push af | push hl | push de | push bc
+        ld a,e
+        ex de,hl
+            call PatternAddress
+        ex de,hl
+        di
+            ld a,1
+            do
+                push af
+                    out (0f7h),a
+                    ld b,CharHeight
+                    do
+                        ld a,(de)
+                        inc de
+                        ld (hl),a
+                        inc hl
+                        ld a,(de)
+                        inc de
+                        ld (hl),a
+                        dec hl
+                        ld a,l
+                        add a,VramWidth
+                        ld l,a
+                        ld a,h
+                        adc a,0
+                        ld h,a
+                    dwnz
+                    ld bc,-VramWidth*CharHeight
+                    add hl,bc
+                pop af
+                inc a
+                cp 4
+            while nz | wend
+            xor a
+            out (0f7h),a
+        ei
+    pop bc | pop de | pop hl | pop af
+    inc hl | inc hl
+ret
+
+
+; void VVramToVram();
+VVramToVram_: public VVramToVram_
+    push af | push hl | push de | push bc
+        ld de,VVram_
+        ld hl,Backup
+        exx
+            ld hl,VramTop
+        exx
+        ld c, VVramHeight
+        do
+            ld b,VVramWidth
+            do 
+                push bc 
+                    ld a,(de)
+                    inc de
+                    cp (hl)
+                    if nz
+                        ld (hl),a
+                        exx
+                            push de
+                                ld e,a
+                                call Put_
+                            pop de
+                        exx
+                    else
+                        exx
+                            inc hl
+                            inc hl
+                        exx
+                    endif
+                    inc hl
+                pop bc
+            dwnz
+            exx
+                push bc
+                    ld bc,VramRowSize-VVramWidth*2
+                    add hl,bc
+                pop bc
+            exx
+            dec c
+        while nz | wend
+    pop bc | pop de | pop hl | pop af
+ret
