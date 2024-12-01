e2f6ef9b9831faee4a2fd18ea60cd248e9ca043a
いぬふと <mxb03523@nifty.com>
2024-12-01 14:27:51 +0900

    Add fm16pi NEURAS

---
+++ fm16pi/neuras/Sprite.c
@@ -0,0 +1,48 @@
+#include "Sprite.h"
+#include "Vram.h"
+#include "VVram.h"
+
+constexpr byte Invalidpattern = 0xff;
+constexpr byte MaxY = VVramHeight * 8 - 16;
+Sprite[SpriteCount] Sprites;
+
+void HideAllSprites()
+{
+    ptr<Sprite> p;
+    for (p : Sprites) {
+        p->pattern = Invalidpattern;
+        p->oldPattern = Invalidpattern;
+    }
+}
+
+
+void ShowSprite(byte index, byte x, byte y, sbyte pattern)
+{
+    ptr<Sprite> p;
+    p = Sprites + index;
+    p->x = x >> 1;
+    p->y = y >> 1;
+    p->pattern = pattern;
+}
+
+
+void HideSprite(byte index) 
+{
+    ptr<Sprite> p;
+    p = Sprites + index;
+    p->pattern = Invalidpattern;
+}
+
+
+void DrawSprites()
+{
+    ptr<Sprite> p;
+    for (p : Sprites) {
+        if (p->pattern != Invalidpattern && p->oldY < MaxY) {
+            DrawSprite(p->x, p->y, p->pattern);
+        }
+        p->oldX = p->x;
+        p->oldY = p->y;
+        p->oldPattern = p->pattern;
+    }
+}
---
+++ fm16pi/neuras/Vram.h
@@ -0,0 +1,20 @@
+constexpr word Vram = 0x0000;
+
+constexpr byte VramWidth = 80;
+constexpr byte VramHeight = 25;
+constexpr byte CharHeight = 8;
+constexpr byte VramStep = 1;
+constexpr byte VramRowSize = VramWidth * CharHeight * VramStep;
+
+constexpr word VramTop = Vram + VramRowSize;
+
+extern void ClearScreen();
+extern word Put(word vram, byte c);
+extern void BeginDraw();
+extern void EndDraw();
+extern void DrawSprite(byte x, byte y, byte pattern);
+
+extern void DrawAll();
+extern word PrintC(word vram, byte c);
+extern void Put2C(word vram, byte c);
+extern void Erase2(word vram);
