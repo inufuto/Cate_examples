949c1e6d4e90a1e9de6e512d9f65ed6a19b5622c
いぬふと <mxb03523@nifty.com>
2025-01-31 22:48:19 +0900

    Add MZ-700 PCG AntiAir

---
+++ mz700pcg/antiair/Chars.h
@@ -0,0 +1,20 @@
+
+constexpr byte Char_Logo = 0x80;
+constexpr byte Char_Fence = 0x90;
+constexpr byte Char_Cannon = 0x91;
+constexpr byte Char_CannonOn = 0x91;
+constexpr byte Char_CannonOff = 0x95;
+constexpr byte Char_Bullet = 0x99;
+constexpr byte Char_Block = 0x9D;
+constexpr byte Char_BlockA = 0x9D;
+constexpr byte Char_BlockB = 0xA1;
+constexpr byte Char_BlockC = 0xA5;
+constexpr byte Char_Bang = 0xA9;
+constexpr byte Char_SmallBang = 0xA9;
+constexpr byte Char_LargeBang = 0xAD;
+constexpr byte Char_Ufo = 0xBD;
+constexpr byte Char_UfoU = 0xBD;
+constexpr byte Char_UfoL = 0xC1;
+constexpr byte Char_End = 0xC5;
+
+constexpr byte Char_Space = 0x0;
