7bf1c558c49a4ea1114101b777f6e78232898ef6
いぬふと <mxb03523@nifty.com>
2025-02-01 16:54:23 +0900

    Add MZ-700+PCG NEURAS

---
+++ mz700pcg/neuras/Chars.h
@@ -0,0 +1,24 @@
+constexpr byte Char_Space = 0x0;
+
+constexpr byte Char_Solver = 0x80;
+constexpr byte Char_Solver_Left = 0x80;
+constexpr byte Char_Solver_Right = 0x88;
+constexpr byte Char_Solver_Up = 0x90;
+constexpr byte Char_Solver_Down = 0x98;
+constexpr byte Char_Monster = 0xA0;
+constexpr byte Char_Monster_Left = 0xA0;
+constexpr byte Char_Monster_Right = 0xA8;
+constexpr byte Char_Monster_Up = 0xB0;
+constexpr byte Char_Monster_Down = 0xB8;
+constexpr byte Char_Card = 0xC0;
+constexpr byte Char_End = 0xF8;
+
+constexpr byte Char_Wall = 0x30;
+constexpr byte Char_Title = 0x40;
+constexpr byte Char_N = 0x40;
+constexpr byte Char_E = 0x44;
+constexpr byte Char_U = 0x48;
+constexpr byte Char_R = 0x4C;
+constexpr byte Char_A = 0x50;
+constexpr byte Char_S = 0x54;
+constexpr byte Char_End2 = 0x58;
