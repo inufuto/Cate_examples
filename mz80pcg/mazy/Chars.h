45bf393fad6d79d5e440a608b86f6e674588b979
いぬふと <mxb03523@nifty.com>
2025-02-05 08:33:55 +0900

    Add MZ-80C+PCG MAZY

---
+++ mz80pcg/mazy/Chars.h
@@ -0,0 +1,47 @@
+constexpr byte Char_Space = 0x0;
+constexpr byte Char_Wall = 0x5A;
+
+constexpr byte Char_Knife = 0x80;
+constexpr byte Char_Knife_Left = 0x80;
+constexpr byte Char_Knife_Right = 0x81;
+constexpr byte Char_Knife_Up = 0x82;
+constexpr byte Char_Knife_Down = 0x83;
+constexpr byte Char_Man = 0x84;
+constexpr byte Char_Man_Left = 0x84;
+constexpr byte Char_Man_Left_Stop = 0x84;
+constexpr byte Char_Man_Left0 = 0x88;
+constexpr byte Char_Man_Left1 = 0x8C;
+constexpr byte Char_Man_Left2 = 0x90;
+constexpr byte Char_Man_Left3 = 0x94;
+constexpr byte Char_Man_Right = 0x98;
+constexpr byte Char_Man_Right_Stop = 0x98;
+constexpr byte Char_Man_Right0 = 0x9C;
+constexpr byte Char_Man_Right1 = 0xA0;
+constexpr byte Char_Man_Right2 = 0xA4;
+constexpr byte Char_Man_Right3 = 0xA8;
+constexpr byte Char_Man_Up = 0xAC;
+constexpr byte Char_Man_Up_Stop = 0xAC;
+constexpr byte Char_Man_Up0 = 0xB0;
+constexpr byte Char_Man_Up1 = 0xB4;
+constexpr byte Char_Man_Up2 = 0xB8;
+constexpr byte Char_Man_Up3 = 0xBC;
+constexpr byte Char_Man_Down = 0xC0;
+constexpr byte Char_Man_Down_Stop = 0xC0;
+constexpr byte Char_Man_Down0 = 0xC4;
+constexpr byte Char_Man_Down1 = 0xC8;
+constexpr byte Char_Man_Down2 = 0xCC;
+constexpr byte Char_Man_Down3 = 0xD0;
+constexpr byte Char_Man_Die2 = 0xD4;
+constexpr byte Char_Man_Die3 = 0xD8;
+constexpr byte Char_Chaser = 0xDC;
+constexpr byte Char_Disturber = 0xE0;
+constexpr byte Char_Goal = 0xE4;
+constexpr byte Char_Bang = 0xE8;
+constexpr byte Char_Title = 0xEC;
+constexpr byte Char_M = 0xEC;
+constexpr byte Char_A = 0xF0;
+constexpr byte Char_Z = 0xF4;
+constexpr byte Char_Y = 0xF8;
+constexpr byte Char_End = 0xFC;
+
+
