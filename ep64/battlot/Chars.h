9b7b731f565063314b25fb3f5dc5ac60737e6be7
いぬふと <mxb03523@nifty.com>
2025-02-09 19:51:33 +0900

    Add Enterprise64 BATTLOT

---
+++ ep64/battlot/Chars.h
@@ -0,0 +1,16 @@
+
+constexpr byte Char_Space = 0x0;
+constexpr byte Char_Ascii = 0x0;
+constexpr byte Char_Logo = 0x40;
+constexpr byte Char_Fence = 0x50;
+constexpr byte Char_Wall = 0x52;
+constexpr byte Char_HardWall = 0x56;
+constexpr byte Char_Meter = 0x57;
+constexpr byte Char_Bullet = 0x59;
+constexpr byte Char_MyRobo = 0x5B;
+constexpr byte Char_EnemyRobo = 0x7B;
+constexpr byte Char_SmallBang = 0x9B;
+constexpr byte Char_LargeBang = 0x9F;
+constexpr byte Char_MyFort = 0xAF;
+constexpr byte Char_EnemyFort = 0xBB;
+constexpr byte Char_End = 0xC7;
---
+++ ep64/battlot/VramA.asm
@@ -0,0 +1,143 @@
+include "Vram.inc"
+include "VVram.inc"
+
+ext VVramFront_
+ext CharPattern
+
+VramTop equ Vram+VramRowSize*1
+
+dseg
+Backup:
+    defs VVramWidth*VVramHeight
+
+
+cseg
+LptSource: public LptSource
+    defb 256-200
+    defb 52h
+    defb 11,51
+    defw Vram,0
+	defb	00h, 0abh, 49h, 11h, 57h, 5eh, 0dbh, 0c7h
+    defb -50,12h,63,0,0,0,0,0,0,0,0,0,0,0,0,0
+    defb -3,16,63,0,0,0,0,0,0,0,0,0,0,0,0,0
+    defb -4,16,6,63,0,0,0,0,0,0,0,0,0,0,0,0
+    defb -1,90h,63,32,0,0,0,0,0,0,0,0,0,0,0,0
+    defb 252,12h,6,63,0,0,0,0,0,0,0,0,0,0,0,0
+    defb -50,13h,63,0,0,0,0,0,0,0,0,0,0,0,0,0
+
+InitVram: public InitVram
+    ld a,1fh | out (80h),a ; Fixbias 
+
+    ld hl,Lpt/16
+    ld a,l | out (82h),a
+    ld a,h | or 0c0h | out (83h),a
+
+    ld hl,LptSource
+    ld de,Lpt
+    ld bc,080h
+    ldir
+ret
+
+
+; void ClearScreen();
+cseg
+Clear:
+    do
+        ld (hl),0
+        inc hl
+        dec bc
+        ld a,c
+        or b
+    while nz | wend
+ret
+ClearScreen_:   public ClearScreen_
+    push af | push hl | push de | push bc
+        ld hl,Vram
+        ld de,Vram+1
+        ld bc,VramRowSize*VramHeight-1
+        ld (hl),0
+        ldir
+
+        ld hl,Backup
+        ld bc,VVramWidth*VVramHeight
+        call Clear
+    pop bc | pop de | pop hl | pop af
+ret
+
+; word Put(word vram, byte c);
+cseg
+Put_: public Put_
+    push af | push hl | push de | push bc
+        ld a,e
+        ex de,hl
+            push de
+                ld l,a
+                ld h,0
+                add hl,hl ;*2
+                add hl,hl ;*4
+                add hl,hl ;*8
+                add hl,hl ;*16
+                ld de,CharPattern
+                add hl,de
+            pop de
+        ex de,hl
+        ld b,CharHeight
+        do
+            ld a,(de) | inc de
+            ld (hl),a | inc hl
+            ld a,(de) | inc de
+            ld (hl),a
+            push bc
+                ld bc,VramWidth-1
+                add hl,bc
+            pop bc
+        dwnz
+    pop bc | pop de | pop hl | pop af
+    inc hl | inc hl
+ret
+
+
+; void VVramToVram();
+cseg
+VVramToVram_: public VVramToVram_
+    push af | push hl | push de | push bc
+        ld de,VVramFront_
+        ld hl,Backup
+        exx
+            ld hl,VramTop
+        exx
+        ld c,VVramHeight
+        do
+            ld b,VVramWidth
+            do 
+                push bc 
+                    ld a,(de)
+                    inc de
+                    cp (hl)
+                    if nz
+                        ld (hl),a
+                        exx
+                            push de
+                                ld e,a
+                                call Put_
+                            pop de
+                        exx
+                    else
+                        exx
+                            inc hl
+                            inc hl
+                        exx
+                    endif
+                    inc hl
+                pop bc
+            dwnz
+            exx
+                push bc
+                    ld bc,VramRowSize-VVramWidth*2
+                    add hl,bc
+                pop bc
+            exx
+            dec c
+        while nz | wend
+    pop bc | pop de | pop hl | pop af
+ret
