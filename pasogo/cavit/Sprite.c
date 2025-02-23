7a992699a36edf7f2c26b81558987e5c2112f811
いぬふと <mxb03523@nifty.com>
2025-02-23 14:32:16 +0900

    Add PASOGO CAVIT

---
+++ pasogo/cavit/Sprite.c
@@ -0,0 +1,58 @@
+#include "Movable.h"
+#include "Sprite.h"
+#include "Vram.h"
+
+constexpr byte Invalidpattern = 0xff;
+constexpr byte MaxY = VramHeight * CharHeight - CharHeight * 2;
+
+struct Element {
+    byte x, y, pattern;
+};
+struct Sprite {
+    Element now;
+    Element old0;
+    Element old1;
+};
+Sprite[SpriteCount] Sprites;
+
+void HideAllSprites()
+{
+    ptr<Sprite> p;
+    for (p : Sprites) {
+        p->now.pattern = Invalidpattern;
+        p->old0.pattern = Invalidpattern;
+        p->old1.pattern = Invalidpattern;
+    }
+}
+
+
+void ShowSprite(byte index, byte x, byte y, byte pattern)
+{
+    ptr<Sprite> p;
+    p = Sprites + index;
+    p->now.x = x;
+    p->now.y = y;
+    p->now.pattern = pattern;
+}
+
+
+void HideSprite(byte index) 
+{
+    ptr<Sprite> p;
+    p = Sprites + index;
+    p->now.pattern = Invalidpattern;
+}
+
+
+void DrawSprites()
+{
+    ptr<Sprite> p;
+    for (p : Sprites) {
+        if (p->now.pattern != Invalidpattern) {
+            DrawSprite(p->now.x, p->now.y, p->now.pattern);
+        }
+        p->old0.x = p->now.x;
+        p->old0.y = p->now.y;
+        p->old0.pattern = p->now.pattern;
+    }
+}
---
+++ pasogo/cavit/Vram.c
@@ -0,0 +1,33 @@
+#include "Vram.h"
+#include "Chars.h"
+#include "Movable.h"
+#include "Sprite.h"
+#include "Sound.h"
+
+void DrawAll()
+{
+    BeginDraw();
+    DrawSprites();
+    EndDraw();
+    CallSound();
+}
+
+
+word PrintC(word vram, byte c)
+{
+    Put(vram, c - ' ');
+    return vram + VramStep;
+}
+
+
+void Put2C(word vram, byte c)
+{
+    repeat (2) {
+        repeat (2) {
+            Put(vram, c);
+            ++c;
+            vram += VramStep;
+        }
+        vram += VramRowSize - 2 * VramStep;
+    }
+}
