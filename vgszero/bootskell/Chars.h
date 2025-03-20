073c7489f5bb6941a5034a50a05228893c018814
いぬふと <mxb03523@nifty.com>
2025-03-20 22:34:57 +0900

    Add VGS-ZERO Bootskell

---
+++ vgszero/bootskell/Chars.h
@@ -0,0 +1,14 @@
+
+constexpr byte Char_Space = 0x0;
+constexpr byte Char_Ascii = 0x0;
+constexpr byte Char_Logo = 0x40;
+constexpr byte Char_Wall = 0x50;
+constexpr byte Char_Block = 0x54;
+constexpr byte Char_Remain = 0x78;
+constexpr byte Char_End = 0x7C;
+
+constexpr byte Pattern_Man = 0;
+constexpr byte Pattern_Monster = 12;
+constexpr byte Pattern_Block = 24;
+constexpr byte Pattern_Point = 25;
+constexpr byte Pattern_End = 29;
---
+++ vgszero/bootskell/SpriteA.asm
@@ -0,0 +1,59 @@
+include "vgszero.inc"
+include "Vram.inc"
+include "Chars.inc"
+
+ext HScroll_
+ext Sprites_
+
+SpriteCount equ 18
+
+cseg
+UpdateSprites_: public UpdateSprites_
+    push af | push hl | push de | push bc
+        ld a,(HScroll_) | ld c,a
+
+        ld de,Sprites_
+        ld hl,OAM
+        ld b,SpriteCount
+        do
+            inc de | inc de
+            ld a,(de)
+            dec de | dec de
+            inc a
+            if nz
+                ld a,(de) | inc de ; y
+                add a,CharHeight*3
+                ld (hl),a | inc hl
+
+                ld a,(de) | inc de ; x
+                sub c
+                ld (hl),a | inc hl
+
+                ld a,(de) | inc de ; pattern
+                push bc
+                    add a,a ;*2
+                    ld b,a
+                    and 0fh
+                    ld c,a
+                    ld a,b
+                    and 0f0h
+                    add a,a ;*2
+                    add a,c
+                    add a,Char_End
+                pop bc
+                ld (hl),a | inc hl
+
+                ld a,80h
+            else
+                inc de | inc de | inc de
+                inc hl | inc hl | inc hl
+                xor a
+            endif
+            ld (hl),a | inc hl ; attribute
+            ld (hl),1 | inc hl ; heightMinus1 
+            ld (hl),1 | inc hl ; widthMinus1
+            ld (hl),PatternBank | inc hl ; bank
+            inc hl
+        dwnz
+    pop bc | pop de | pop hl | pop af
+ret
