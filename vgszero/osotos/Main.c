37eaa25beb8f4c915415b4b42938d252141ff884
いぬふと <mxb03523@nifty.com>
2025-03-22 07:35:02 +0900

    Add VGS-ZERO OSOTOS

---
+++ vgszero/osotos/Main.c
@@ -0,0 +1,149 @@
+#include "Main.h"
+#include "ScanKeys.h"
+#include "Vram.h"
+#include "Sound.h"
+#include "Sprite.h"
+#include "Status.h"
+#include "Stage.h"
+#include "Man.h"
+#include "Monster.h"
+#include "Block.h"
+#include "Point.h"
+
+constexpr byte MaxTimeDenom = 50 / (8 / CoordRate);
+constexpr byte BonusRate = 5;
+
+word Score;
+word HiScore;
+byte RemainCount;
+byte CurrentStage;
+byte StageTime;
+byte ItemCount;
+static byte Clock;
+static sbyte monsterNum;
+
+extern void _deb();
+
+static void ShowStage()
+{
+    sbyte x;
+    x = HScroll;
+    while (HScroll < 8) {
+        ++HScroll;
+        WaitTimer(5);
+    }
+    while (HScroll > -8) {
+        --HScroll;
+        WaitTimer(5);
+    }
+    while (HScroll < x) {
+        ++HScroll;
+        WaitTimer(5);
+    }
+}
+
+void Main()
+{
+    Sound_Hit();
+
+    HiScore = 0;
+    Score = 0;
+    CurrentStage = 0;
+    RemainCount = 3;
+    StageTime = 0;
+    
+    title:
+    Title();
+    byte key;
+    do {
+        // if (ScanStop()) goto exit;
+        key = ScanKeys();
+    } while ((key & (Keys_Button0 | Keys_Button1)) == 0);
+    
+    play:
+    Score = 0;
+    if ((key & Keys_Button1) == 0 && (key & Keys_Dir) == 0) {
+        CurrentStage = 0;
+    }
+    RemainCount = 3;
+    stage:
+    InitStage();
+    try:
+    InitTrying();
+    Clock = 0;
+    monsterNum = 0;
+    byte timeDenom = MaxTimeDenom;
+    Sound_Start();     
+    ShowStage();
+    WaitSound();
+    StartBGM();
+    do {
+        //     if (ScanStop()) goto exit;
+        if ((Clock & 3) == 0) {
+            MoveMan();
+            if (monsterNum >= 0) {
+                MoveMonsters();
+                monsterNum -= 10;
+            }
+            monsterNum += 6;
+            
+            --timeDenom;
+            if (timeDenom == 0) {
+                --StageTime;
+                timeDenom = MaxTimeDenom;
+                PrintTime();
+                if (StageTime == 0) {
+                    PrintTimeUp();
+                    lose:
+                    StopBGM();
+                    LooseMan();
+                    --RemainCount;
+                    if (RemainCount > 0) {
+                        goto try;
+                    } 
+                    PrintGameOver();
+                    Sound_GameOver();
+                    goto title;
+                }
+            }
+            UpdateBlocks();
+            UpdatePoints();
+            WaitTimer(8 / CoordRate);
+        }
+        if ((Clock & 1) == 0) {
+            MoveBlocks();
+        }
+        UpdateSprites();
+        ++Clock;
+        if ((Man.status & Movable_Live) == 0) {
+            goto lose;
+        }
+    } while (ItemCount != 0);
+    UpdateSprites();
+    WaitTimer(30);
+    StopBGM();
+    WaitTimer(10);
+    Sound_Clear();
+    while (StageTime >= BonusRate) {
+        AddScore(5);
+        StageTime -= BonusRate;
+        PrintTime();
+        Sound_Beep();
+    }
+    StageTime = 0;
+    PrintStatus();
+    ++CurrentStage;
+    goto stage;
+}
+
+
+void AddScore(word pts) 
+{
+    Score += pts;
+    if (Score > HiScore) {
+        HiScore = Score;
+    }
+    PrintScore();
+}
+
+void _deb(){}
---
+++ vgszero/osotos/Stage.c
@@ -0,0 +1,184 @@
+#include "Stage.h"
+#include "Stages.h"
+#include "Main.h"
+#include "Vram.h"
+#include "Sprite.h"
+#include "Chars.h"
+#include "Status.h"
+#include "Block.h"
+#include "Man.h"
+#include "Monster.h"
+#include "CopyMemory.h"
+#include "Point.h"
+#include "Math.h"
+
+extern void _deb();
+
+ptr<Stage> pStage;
+byte[ColumnCount * FloorCount] CellMap;
+
+void InitStage() 
+{
+    {
+        ptr<Stage> p = Stages;
+        byte i = 0;
+        byte j = 0;
+        while (i < CurrentStage) {
+            ++p;
+            ++i;
+            ++j;
+            if (j >= StageCount) {
+                p = Stages;
+                j = 0;
+            }
+        }
+        pStage = p;
+    }
+}
+
+
+static ptr<byte> DrawCell(ptr<byte> pVram, byte b, byte y)
+{
+    static const byte[] LadderChars = {
+        Char_Ladder + 0, Char_Ladder + 1, Char_Ladder + 0, Char_Ladder + 1,
+    };
+    static const byte[] FloorChars = {
+        Char_Floor, Char_Floor, Char_Space, Char_Space,
+    };
+    static const byte[] FloorLadderChars = {
+        Char_Floor, Char_Floor, Char_Ladder + 0, Char_Ladder + 1,
+    };
+    if (y >= StatusHeight) {
+        if ((b & Cell_Block) != 0) {
+            Put2C(pVram, Char_Block);
+        }
+        else if ((b & Cell_Ceiling) != 0) {
+            Put2S(pVram, FloorChars);
+        }
+        else if ((b & (Cell_Down | Cell_Up)) != 0) {
+            Put2S(pVram, LadderChars);
+        }
+        else {
+            Erase2(pVram);
+        }
+    }
+    pVram += VramRowSize * CellSize;
+    b >>= 4;
+    if ((b & Cell_Block) != 0) {
+        Put2C(pVram, Char_Block);
+    }
+    else if ((b & Cell_Item) != 0) {
+        Put2C(pVram, Char_Item);
+    }
+    else if ((b & (Cell_Down | Cell_Up)) != 0) {
+        Put2S(pVram, LadderChars);
+    }
+    else {
+        Erase2(pVram);
+    }
+    return pVram + CellSize * VramStep - VramRowSize * CellSize;
+}
+
+
+void InitTrying()
+{
+    RndIndex = 0;
+    ItemCount = 0;
+    {
+        FillMemory(CellMap, ColumnCount * FloorCount, 0);
+
+        constexpr byte Mask = 3;
+        constexpr byte Floor = 1;
+        constexpr byte Ladder = 2;
+        constexpr byte Item = 3;
+
+        byte floor = 0;
+        ptr<byte> pCell = CellMap;
+        ptr<byte> pUpper = pStage->bytes;
+        ptr<byte> pLower = pUpper + ColumnCount / ColumnsPerByte;
+        repeat (FloorCount) {
+            repeat (ColumnCount / ColumnsPerByte) {
+                byte upper = *pUpper; ++pUpper;
+                byte lower = *pLower; ++pLower;
+                repeat (ColumnsPerByte) {
+                    byte bits = upper & Mask;
+                    if (bits == Floor && floor < FloorCount - 1) {
+                        pCell[ColumnCount] |= Cell_Ceiling;
+                    }
+                    if (bits == Ladder) {
+                        *pCell |= (Cell_Up | Cell_Down) | (Cell_Up << 4);
+                        if (floor < FloorCount - 1) {
+                            pCell[ColumnCount] |= Cell_Ceiling;
+                        }
+                    }
+                    if (floor < FloorCount - 1 && (lower & Mask) == Ladder) {
+                        *pCell |= Cell_Down << 4;
+                        if (floor < FloorCount - 1) {
+                            pCell[ColumnCount] &= ~Cell_Ceiling;
+                        }
+                    }
+                    if (bits == Item) {
+                        *pCell |= Cell_Item << 4;
+                        if (floor < FloorCount - 1) {
+                            pCell[ColumnCount] |= Cell_Ceiling;
+                        }
+                        ++ItemCount;
+                    }
+                    ++pCell;
+                    upper >>= 2;
+                    lower >>= 2;
+                }
+            }
+            ++floor;
+        }
+    }
+    {
+        StageTime = 40;
+        byte i = ItemCount;
+        do {
+            StageTime += 8;
+            if (StageTime > 240) break;
+            --i;
+        } while (i != 0);
+    }
+    HideAllSprites();
+    ClearScreen();
+    PrintStatus();
+    InitBlocks();
+    {
+        ptr<byte> pCell = CellMap;
+        ptr<byte> pVram = VramBack;
+        byte y = 0;
+        repeat (FloorCount) {
+            repeat (ColumnCount) {
+                byte cell = *pCell; ++pCell;
+                pVram = DrawCell(pVram, cell, y);
+            }
+            y += FloorHeight;
+            pVram += VramRowSize * (FloorHeight - 1);
+        }
+    }
+    InitMan();
+    InitMonsters();
+    InitPoints();
+    UpdateSprites();
+}
+
+
+byte GetCell(byte column, byte row)
+{
+    byte b = *CellMapPtr(column, row);
+    if ((row & 1) != 0) {
+        b >>= 4;
+    }
+    return b & 0x0f;
+}
+
+
+void DrawCellCR(byte column, byte row)
+{
+    row &= 0xfe;
+    byte b = *CellMapPtr(column, row);
+    byte y = row << CellShift;
+    DrawCell(VramBack + VramOffset(column << CellShift, y), b, y);
+}
