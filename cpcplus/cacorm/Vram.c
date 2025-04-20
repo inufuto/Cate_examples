ffa1e3c5c6c4427434cd8df97fc99ef994a4bd24
いぬふと <mxb03523@nifty.com>
2025-04-20 17:07:54 +0900

    Add CPC464Plus Cacorm

---
+++ cpcplus/cacorm/Vram.c
@@ -0,0 +1,48 @@
+#include "Vram.h"
+#include "CopyMemory.h"
+#include "Chars.h"
+#include "Movable.h"
+#include "Sprite.h"
+
+ptr<byte> PrintC(ptr<byte> pVram, byte c)
+{
+    return Put(pVram, c - ' ');
+}
+
+
+ptr<byte> Put2C(ptr<byte> pVram, byte c)
+{
+    repeat (2) {
+        repeat (2) {
+            pVram = Put(pVram, c);
+            ++c;
+        }
+        pVram += VramRowSize - 2 * VramStep;
+    }
+    return pVram + 2 * VramStep - VramRowSize * 2;
+}
+
+
+ptr<byte> Erase2(ptr<byte> pVram)
+{
+    repeat (2) {
+        repeat (2) {
+            pVram = Put(pVram, Char_Space);
+        }
+        pVram += VramRowSize - 2 * VramStep;
+    }
+    return pVram + 2 * VramStep - VramRowSize * 2;
+}
+
+
+ptr<byte> Put2S(ptr<byte> pVram, ptr<byte> pChars)
+{
+    repeat (2) {
+        repeat (2) {
+            pVram = Put(pVram, *pChars);
+            ++pChars;
+        }
+        pVram += VramRowSize - 2 * VramStep;
+    }
+    return pVram + 2 * VramStep - VramRowSize * 2;
+}
