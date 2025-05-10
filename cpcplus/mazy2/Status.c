4a93d2eced93e22ad365279249ce6da3df042920
いぬふと <mxb03523@nifty.com>
2025-05-10 18:20:32 +0900

    Add CPC464Plus MAZY2

---
+++ cpcplus/mazy2/Status.c
@@ -0,0 +1,157 @@
+#include "Status.h"
+#include "Vram.h"
+#include "Stage.h"
+#include "Main.h"
+#include "Sprite.h"
+#include "Print.h"
+#include "Chars.h"
+
+static ptr<byte> PrintS(ptr<byte> pVram, ptr<byte> p)
+{
+    byte c;
+    while ((c = *p) != 0) {
+        pVram = PrintC(pVram, c);
+        ++p;
+    }
+    return pVram;
+}
+
+void PrintStatus() 
+{
+    PrintS(Vram + VramRowSize * 1 + 32 * VramStep, "SCORE");
+    PrintS(Vram + VramRowSize * 5 + 32 * VramStep, "HI-SCORE");
+    PrintS(Vram + VramRowSize * 9 + 32 * VramStep, "STAGE");
+    PrintByteNumber2(Vram + VramRowSize * 9 + 38 * VramStep, CurrentStage + 1);
+    PrintS(Vram + VramRowSize * 12 + 32 * VramStep, "TIME");
+
+    {
+        ptr<byte> pVram;
+        pVram = Vram + VramRowSize * 21 + 32 * VramStep;
+        if (RemainCount > 1) {
+            byte i;
+            i = RemainCount - 1;
+            if (i > 2) {
+                Put2C(pVram, Char_Remain);
+                pVram += 2 * VramStep;
+                pVram = Put(pVram, Char_Space);
+                pVram = Put(pVram, Char_Space);
+                pVram += VramRowSize - 2 * VramStep;
+                pVram = Put(pVram, i + 0x10);
+                pVram = Put(pVram, Char_Space);
+            }
+            else {
+                do {
+                    Put2C(pVram, Char_Remain);
+                    pVram += 2 * VramStep;
+                    --i;
+                } while (i > 0);
+            }
+        }
+    }
+    PrintScore();
+    PrintTime();
+    PrintHeldKnives();
+}
+
+void PrintScore()
+{
+    ptr<byte> pVram;
+    
+    pVram = PrintNumber5(Vram + VramRowSize * 2 + 34 * VramStep, Score);
+    PrintC(pVram, '0');
+
+    pVram = PrintNumber5(Vram + VramRowSize * 6 + 34 * VramStep, HiScore);
+    PrintC(pVram, '0');
+}
+
+void PrintTime()
+{
+    PrintByteNumber3(Vram + VramRowSize * 12 + 37 * VramStep, StageTime);
+}
+
+void PrintHeldKnives()
+{
+    ptr<byte> pVram;
+    pVram = Vram + VramRowSize * 19 + 32 * VramStep;
+    byte i = HeldKnifeCount;
+    while (i != 0) {
+        pVram = Put(pVram, Char_Knife) + 1;
+        --i;
+    }
+    Put(pVram, Char_Space);
+}
+
+void PrintGameOver()
+{
+    PrintS(Vram + VramRowSize * 11 + 12 * VramStep, "GAME OVER");
+}
+
+void PrintTimeUp() 
+{
+    PrintS(Vram + VramRowSize * 11 + 13 * VramStep, "TIME UP");
+}
+
+void Title()
+{
+    ClearScreen(); 
+    HideAllSprites();
+    PrintStatus();
+    {
+        static const byte[] TitleBytes = {
+            //	M
+            0x4c, 0x47, 0x47, 0x4b, 
+            0x4c, 0x43, 0x43, 0x4f, 
+            0x4c, 0x43, 0x43, 0x4f, 
+            0x44, 0x41, 0x41, 0x45, 
+            //	A
+            0x40, 0x4e, 0x4d, 0x42, 
+            0x4c, 0x43, 0x40, 0x4f, 
+            0x4c, 0x47, 0x45, 0x4f, 
+            0x44, 0x41, 0x40, 0x45, 
+            //	Z
+            0x44, 0x45, 0x4d, 0x47, 
+            0x40, 0x48, 0x47, 0x40, 
+            0x48, 0x47, 0x40, 0x40, 
+            0x44, 0x45, 0x45, 0x45, 
+            //	Y
+            0x4c, 0x43, 0x4c, 0x43, 
+            0x44, 0x4b, 0x4e, 0x41, 
+            0x40, 0x4c, 0x43, 0x40, 
+            0x40, 0x44, 0x41, 0x40, 
+            //	2
+            0x48, 0x47, 0x45, 0x4b, 
+            0x40, 0x48, 0x4e, 0x47, 
+            0x48, 0x4f, 0x45, 0x40, 
+            0x44, 0x45, 0x45, 0x45, 
+        };
+        constexpr byte LogoLength = 5;
+        ptr<byte> p;
+        ptr<byte> pVram;
+        pVram = Vram + VramRowSize * 8 + (32 - LogoLength * 4) / 2 * VramStep;
+        p = TitleBytes;
+        repeat (LogoLength) {
+            repeat (4) {
+                repeat (4) {
+                    pVram = Put(pVram, *p);
+                    ++p;
+                }
+                pVram += VramRowSize - 4 * VramStep;
+            }
+            pVram += 4 * VramStep - VramRowSize * 4;
+        }
+    }
+    PrintS(Vram + VramRowSize * 20 + 9 * VramStep, "PUSH SPACE KEY");
+    PrintS(Vram + VramRowSize * 21 + 9 * VramStep, "OR SHIFT KEY");
+    PrintS(Vram + VramRowSize * 23 + 20 * VramStep, "INUFUTO 2025");
+    // {
+    //     ptr<byte> pVram = Vram;
+    //     byte c = 0;
+    //     repeat (Char_End) {
+    //         pVram = Put(pVram, c);
+    //         ++c;
+    //         if ((c & 15) == 0) {
+    //             pVram += VramRowSize - 16 * VramStep;
+    //         }
+    //     }
+    // }
+}
---
+++ cpcplus/mazy2/Vram.c
@@ -0,0 +1,19 @@
+#include "Vram.h"
+
+ptr<byte> PrintC(ptr<byte> pVram, byte c)
+{
+    return Put(pVram, c - ' ');
+}
+
+
+ptr<byte> Put2C(ptr<byte> pVram, byte c)
+{
+    repeat (2) {
+        repeat (2) {
+            pVram = Put(pVram, c);
+            ++c;
+        }
+        pVram += VramRowSize - 2 * VramStep;
+    }
+    return pVram + 2 * VramStep - VramRowSize * 2;
+}
