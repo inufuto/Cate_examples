557ec13fe6c336e55e14b08bd5120ea4c3a1e816
いぬふと <mxb03523@nifty.com>
2025-05-30 22:34:04 +0900

    Add MZ-700 MAZY2

---
+++ mz700/mazy2/Chars.asm
@@ -0,0 +1,34 @@
+cseg
+
+Chars: public Chars
+;	logo
+	defb	00h, 50h, 0f1h, 50h, 0f4h, 50h, 0f5h, 50h
+	defb	0f2h, 50h, 0f3h, 50h, 0f6h, 50h, 0f7h, 50h
+	defb	0f8h, 50h, 0f9h, 50h, 0fch, 50h, 0fdh, 50h
+	defb	0fah, 50h, 0fbh, 50h, 0feh, 50h, 0ffh, 50h
+;	1c
+	defb	43h, 50h, 45h, 70h, 40h, 70h, 50h, 70h
+	defb	80h, 70h
+;	point
+	defb	20h, 70h, 21h, 70h, 22h, 70h, 24h, 70h
+	defb	28h, 70h
+;	2c
+	defb	0ffh, 60h, 00h, 70h, 0f6h, 70h, 0f4h, 70h
+	defb	0ffh, 60h, 0f4h, 70h, 0f6h, 70h, 0f8h, 70h
+	defb	0ffh, 60h, 00h, 00h, 0fah, 70h, 0f4h, 70h
+	defb	00h, 70h, 0ffh, 60h, 0f8h, 70h, 0f9h, 70h
+	defb	0f8h, 70h, 0ffh, 60h, 0f4h, 70h, 0f9h, 70h
+	defb	00h, 00h, 0ffh, 60h, 0f8h, 70h, 0f5h, 70h
+	defb	0ffh, 60h, 0f6h, 70h, 00h, 70h, 0f2h, 70h
+	defb	0ffh, 60h, 0f6h, 70h, 0f2h, 70h, 0f8h, 70h
+	defb	0ffh, 60h, 0fch, 70h, 00h, 70h, 0f2h, 70h
+	defb	00h, 70h, 0f8h, 70h, 0ffh, 60h, 0f9h, 70h
+	defb	0f8h, 70h, 0f2h, 70h, 0ffh, 60h, 0f9h, 70h
+	defb	00h, 70h, 0f8h, 70h, 0ffh, 60h, 0f3h, 70h
+	defb	0f2h, 70h, 0f6h, 70h, 00h, 00h, 0ffh, 60h
+	defb	0f9h, 70h, 0ffh, 60h, 0f1h, 70h, 00h, 00h
+	defb	0fh, 70h, 0fh, 70h, 0feh, 30h, 0fdh, 30h
+	defb	0d0h, 40h, 0d0h, 40h, 0feh, 20h, 0fdh, 20h
+	defb	0c1h, 14h, 0c1h, 14h, 0c1h, 14h, 0c1h, 14h
+	defb	0c2h, 16h, 0c2h, 16h, 0c2h, 16h, 0c2h, 16h
+	defb	37h, 06h, 2eh, 06h, 37h, 06h, 43h, 60h
---
+++ mz700/mazy2/VramA.asm
@@ -0,0 +1,138 @@
+include "Vram.inc"
+include "VVram.inc"
+include "Chars.inc"
+
+ext VVramBack_, VVramFront_
+ext Chars
+
+dseg
+Backup: defs VVramWidth*VVramHeight
+
+; void ClearScreen();
+cseg
+Clear:
+    ld bc,VramWidth*VramHeight
+    do
+        ld (hl),0
+        inc hl
+        dec bc
+        ld a,c
+        or b
+    while nz | wend
+ret
+ClearScreen_: public ClearScreen_
+    push af | push hl | push bc
+        ld hl,Vram
+        call Clear
+
+        ld hl,Atributes
+        call Clear
+
+        ld hl,Backup
+        call Clear
+
+        ld hl,VVramBack_
+        call Clear
+    pop bc | pop hl | pop af
+ret
+
+
+; void VVramToVram();
+cseg
+VVramToVram_: public VVramToVram_
+    push af | push hl | push de | push bc
+        ld de,VVramFront_
+        ld hl,Backup
+        exx
+            ld de,Vram+VramRowSize*1
+        exx
+        ld c, VVramHeight ; vertical count    
+        do
+            ld b,VVramWidth
+            do 
+                ld a,(de)
+                inc de
+                cp (hl)
+                if nz
+                    ld (hl),a
+                    exx
+                        push hl | push bc
+                            ld l,a
+                            ld h,0
+                            add hl,hl
+                            ld bc,Chars
+                            add hl,bc
+                            ld a,(hl)
+                            inc hl
+                            ld (de),a
+                            ld a,(hl)
+                            set 3,d
+                            ld (de),a
+                            res 3,d
+                        pop bc | pop hl
+                    exx
+                endif
+                exx
+                    inc de
+                exx
+                inc hl
+            dwnz
+            exx
+                ex de,hl
+                    push bc
+                        ld bc,VramRowSize-VVramWidth
+                        add hl,bc
+                    pop bc
+                ex de,hl
+            exx
+            dec c
+        while nz | wend
+    pop bc | pop de | pop hl | pop af
+ret
+
+
+; ptr<byte> PrintC(ptr<byte> address, byte c);
+cseg
+CharTable:
+    defb 0,61h,62h,63h,64h,65h,66h,67h,68h,69h,6bh,6ah,2fh,2ah,2eh,2dh
+    defb 20h,21h,22h,23h,24h,25h,26h,27h,28h,29h,4fh,2ch,51h,2bh,57h,49h
+    defb 55h,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+    defb 16,17,18,19,20,21,22,23,24,25,26,52h,59h,54h,46h,3ch
+PrintC_: public PrintC_
+    push af
+        ld a,e
+        exx
+            ld l,a
+            ld h,0
+            ld de,CharTable - ' '
+            add hl,de
+            ld a,(hl)
+        exx
+        ld (hl),a
+        set 3,h
+        ld (hl),70h
+        res 3,h
+        inc hl
+    pop af
+ret
+
+
+; ptr<byte> Put(ptr<byte> pVram, byte c);
+cseg
+Put_: public Put_
+    push af | push hl | push de | push bc
+        ex de,hl
+            ld h,0
+            add hl,hl
+            ld bc,Chars
+            add hl,bc
+        ex de,hl
+        ld a,(de)
+        inc de
+        ld (hl),a
+        ld a,(de)
+        set 3,h
+        ld (hl),a
+    pop bc | pop de | pop hl | pop af
+    inc hl
+ret
