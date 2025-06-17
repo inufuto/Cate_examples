ec2bb655b0edf327b539110b12c379a196443bfa
いぬふと <mxb03523@nifty.com>
2025-06-17 16:48:16 +0900

    Add DSK

---
+++ msx2/guntus/Loader.asm
@@ -0,0 +1,27 @@
+include "BIOS.inc"
+include "BinSize.inc"
+
+SectorSize equ 512
+SectorCount equ (BinSize+SectorSize-1)/SectorSize
+
+cseg
+    defb 0ebh
+    defs 1dh
+Entry: public Entry
+    ret c
+    ld sp,BinStart
+    ld hl,Loader
+    ld de,8000h
+    ld bc,Tail-Loader
+    ldir
+    jp 8000h
+Loader:
+    xor a
+    ld b,SectorCount
+    ld c,0f9h ; 720K
+    ld de,1
+    ld hl,BinStart
+    call 4010h
+
+    jp BinStart
+Tail:
