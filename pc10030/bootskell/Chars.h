3d1df69d3fa1df4571f0455db3362e75244e2c0c
いぬふと <mxb03523@nifty.com>
2025-06-21 11:18:11 +0900

    Add PC-10030 Bootskell

---
+++ pc10030/bootskell/Chars.h
@@ -0,0 +1,16 @@
+
+constexpr word Char_Space = 0x0;
+constexpr word Char_Ascii = 0x0;
+constexpr word Char_Logo = 0x40;
+constexpr word Char_Fence = 0x50;
+constexpr word Char_Color = 0x52;
+constexpr word Char_Wall = 0x52;
+constexpr word Char_Block = 0x56;
+constexpr word Char_Remain = 0x7A;
+constexpr word Char_End = 0x7E;
+
+constexpr byte Pattern_Man = 0;
+constexpr byte Pattern_Monster = 12;
+constexpr byte Pattern_Block = 24;
+constexpr byte Pattern_Point = 25;
+constexpr byte Pattern_End = 29;
---
+++ pc10030/bootskell/Startup.asm
@@ -0,0 +1,65 @@
+ext InitVram
+ext InitKeys
+ext InitSound
+ext Main_
+
+TimerInt equ 42h
+
+dseg
+TimerCount:
+    defb 0
+count10:
+    defb 0
+
+cseg
+    mov ax,cs | mov ds,ax
+
+    cli
+        in al,02h
+        and al,not 04h
+        out 02h,al
+        mov byte ptr[count10],10
+        
+        xor ax,ax | mov es,ax
+        mov ax,TimerHandler | mov [es:TimerInt*4],ax
+        mov ax,cs | mov [es:TimerInt*4+2],ax
+    sti
+
+    call InitVram
+    call InitKeys
+    call InitSound
+jmp Main_
+
+
+TimerHandler: public TimerHandler
+    push ax
+    push ds
+        mov ax,cs | mov ds,ax
+        
+        mov al,[count10]
+        dec al
+        mov [count10],al
+        if z
+            inc byte ptr[TimerCount]
+            ; call SoundHandler
+            mov byte ptr[count10],10
+        endif
+
+        mov al,20h | out 00h,al ; EOI
+    pop ds
+    pop ax
+iret
+
+
+WaitTimer_: public WaitTimer_
+    push ax
+        mov ah,al
+        do
+            mov al,[TimerCount]
+            cmp al,ah
+        while b | wend
+        cli
+            mov byte ptr [TimerCount],0
+        sti
+    pop ax
+ret
