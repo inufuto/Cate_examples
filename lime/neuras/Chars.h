6ad7662db57375ed4cc35799f665ddc0b1281e74
いぬふと <mxb03523@nifty.com>
2025-06-28 19:13:38 +0900

    Add Limilight NEURAS

---
+++ lime/neuras/Chars.h
@@ -0,0 +1,28 @@
+
+constexpr byte Char_Space = 0;
+constexpr byte Char_Ascii = 0;
+constexpr byte Char_Wall = 64;
+constexpr byte Char_Card = 80;
+constexpr byte Char_N = 136;
+constexpr byte Char_E = 140;
+constexpr byte Char_U = 144;
+constexpr byte Char_R = 148;
+constexpr byte Char_A = 152;
+constexpr byte Char_S = 156;
+constexpr byte Char_Color = 160;
+constexpr byte Char_Solver = 160;
+constexpr byte Char_Monster = 164;
+constexpr byte Char_End = 168;
+
+constexpr byte Pattern_Solver = 0;
+constexpr byte Pattern_Solver_Left = 0;
+constexpr byte Pattern_Solver_Right = 2;
+constexpr byte Pattern_Solver_Up = 4;
+constexpr byte Pattern_Solver_Down = 6;
+constexpr byte Pattern_Monster = 8;
+constexpr byte Pattern_Monster_Left = 8;
+constexpr byte Pattern_Monster_Right = 10;
+constexpr byte Pattern_Monster_Up = 12;
+constexpr byte Pattern_Monster_Down = 14;
+constexpr byte Pattern_Card = 16;
+constexpr byte Pattern_End = 17;
