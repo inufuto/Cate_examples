361eafe9914456cfdf3cac664035755059808f97
いぬふと <mxb03523@nifty.com>
2025-06-28 14:46:22 +0900

    Add Limelight LIFT

---
+++ lime/lift/Vram.c
@@ -0,0 +1,34 @@
+#include "Vram.h"
+#include "Chars.h"
+#include "Sprite.h"
+#include "CopyMemory.h"
+// #include "Sound.h"
+
+void DrawAll()
+{
+    EraseSprites();
+    VVramToVram();
+    DrawSprites();
+    SwitchVram();
+    // CallSound();
+}
+
+
+word PrintC(word vram, byte c)
+{
+    Put(vram, c - ' ');
+    return vram + VramStep;
+}
+
+
+void Put2(word vram, byte c)
+{
+    repeat (2) {
+        repeat (2) {
+            Put(vram, c);
+            ++c;
+            vram += VramStep;
+        }
+        vram += VramRowSize - 2 * VramStep;
+    }
+}
