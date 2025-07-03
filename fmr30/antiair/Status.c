ef91faa2a20a9df168e4532e2f7fe9ca31d18a50
いぬふと <mxb03523@nifty.com>
2025-07-03 23:31:35 +0900

    Add FMR-30 AntiAir

---
+++ fmr30/antiair/Status.c
@@ -0,0 +1,146 @@
+#include "Status.h"
+#include "Vram.h"
+#include "VVram.h"
+#include "Print.h"
+#include "Chars.h"
+#include "Sprite.h"
+
+static word PrintS(word vram, ptr<byte> p)
+{
+    byte c;
+    while ((c = *p) != 0) {
+        vram = PrintC(vram, c);
+        ++p;
+    }
+    return vram;
+}
+
+void PrintStatus()
+{
+    PrintS(Vram + VramRowSize * 1 + WindowWidth * VramStep, "SCORE");
+    PrintS(Vram + VramRowSize * 4 + WindowWidth * VramStep, "HIGH");
+    PrintS(Vram + VramRowSize * 5 + WindowWidth * VramStep, "SCORE");
+    PrintS(Vram + VramRowSize * 8 + WindowWidth * VramStep, "STAGE");
+    {
+        word vram;
+        vram = Vram + VramRowSize * 21 + (WindowWidth + 1) * VramStep;
+        if (RemainCount > 1) {
+            byte i;
+            i = RemainCount - 1;
+            if (i > 2) {
+                vram = Put2C(vram, Char_Remain);
+                Erase2(vram);
+                vram = Put(vram + VramRowSize, i + 0x10);
+            }
+            else {
+                do {
+                    vram = Put2C(vram, Char_Remain);
+                    --i;
+                } while (i > 0);
+            }
+        }
+    }
+    PrintScore();
+    PrintStage();
+}
+
+void PrintScore()
+{
+    word vram;
+    
+    vram = PrintNumber5(Vram + VramRowSize * 2 + VramWidth - 6 * VramStep, Score);
+    PrintC(vram, '0');
+
+    vram = PrintNumber5(Vram + VramRowSize * 6 + VramWidth - 6 * VramStep, HiScore);
+    PrintC(vram, '0');
+}
+
+void PrintStage()
+{
+    PrintByteNumber2(Vram + VramRowSize * 8 + VramWidth - 2 * VramStep, CurrentStage + 1);
+}
+
+
+void PrintGameOver()
+{
+    constexpr byte Left = (WindowWidth - 8) / 2;
+    PrintS(Vram + VramRowSize * (VramHeight / 2) + Left * VramStep, "GAME OVER");
+}
+
+void Title()
+{
+    ClearScreen(); 
+    HideAllSprites();
+    // UpdateSprites();
+    PrintStatus();
+    {
+        static const byte[] TitleBytes = {
+            0x4e, 0x45, 0x4b, 0x4c, 
+            0x4f, 0x40, 0x4f, 0x4c, 
+            0x4f, 0x45, 0x4f, 0x44, 
+            0x45, 0x40, 0x45, 0x40, 
+            
+            0x4b, 0x4c, 0x43, 0x45, 
+            0x47, 0x4f, 0x43, 0x40, 
+            0x41, 0x44, 0x41, 0x40, 
+            0x40, 0x40, 0x40, 0x40, 
+            
+            0x4f, 0x45, 0x4c, 0x43, 
+            0x4f, 0x40, 0x4c, 0x43, 
+            0x45, 0x40, 0x44, 0x41, 
+            0x40, 0x40, 0x40, 0x40, 
+            
+            0x48, 0x47, 0x4d, 0x42, 
+            0x4c, 0x43, 0x4c, 0x43, 
+            0x4c, 0x47, 0x4d, 0x43, 
+            0x44, 0x41, 0x44, 0x41, 
+            
+            0x40, 0x40, 0x40, 0x40, 
+            0x4f, 0x4c, 0x47, 0x4b, 
+            0x4f, 0x4c, 0x4b, 0x47, 
+            0x45, 0x44, 0x41, 0x45, 
+        };
+        constexpr byte LogoLength = 5;
+        constexpr byte LogoX = (WindowWidth - LogoLength * 4) * VramStep / 2;
+        ptr<byte> p;
+        word vram;
+        vram = Vram + VramRowSize * 7 + LogoX;
+        p = TitleBytes;
+        repeat (LogoLength) {
+            repeat (4) {
+                repeat (4) {
+                    vram = Put(vram, *p);
+                    ++p;
+                }
+                vram += VramRowSize - 4 * VramStep;
+            }
+            vram += 4 * VramStep - VramRowSize * 4;
+        }
+    }
+    PrintS(Vram + VramRowSize * 19 + 11 * VramStep, "Z:START");
+    PrintS(Vram + VramRowSize * 20 + 11 * VramStep, "X:CONTINUE");
+    PrintS(Vram + VramRowSize * 22 + (WindowWidth - 12) * VramStep, "INUFUTO 2025");
+    // {
+    //     byte c;
+    //     word vram;
+    //     vram = Vram;
+    //     c = 0;
+    //     repeat (0) {
+    //         vram = Put(vram, c);
+    //         ++c;
+    //         if ((c & 15) == 0) {
+    //             vram += VramRowSize - 16 * VramStep;
+    //         }
+    //     }
+    // }
+}
+
+
+void AddScore(word pts)
+{
+    Score += pts;
+    if (Score > HiScore) {
+        HiScore = Score;
+    }
+    PrintScore();
+}
---
+++ fmr30/antiair/Vram.c
@@ -0,0 +1,57 @@
+#include "Vram.h"
+#include "VVram.h"
+#include "Chars.h"
+#include "Ufo.h"
+#include "Block.h"
+#include "Sprite.h"
+
+extern void _deb();
+
+void DrawAll()
+{
+    if (BackgroundChanged) {
+        BackgroundChanged = false;
+        DrawBackground();
+        DrawUfos();
+    }
+    else if (UfoMoved) {
+        UfoMoved = false;
+        DrawUfos();
+    }
+    EraseSprites();
+    VVramToVram();
+    DrawSprites();
+    SwitchVram();
+}
+
+
+word PrintC(word vram, byte c)
+{
+    Put(vram, c - ' ');
+    return vram + VramStep;
+}
+
+
+word Put2C(word vram, byte c)
+{
+    repeat (2) {
+        repeat (2) {
+            vram = Put(vram, c);
+            ++c;
+        }
+        vram += VramRowSize - 2 * VramStep;
+    }
+    return vram + 2 * VramStep - VramRowSize * 2;
+}
+
+
+word Erase2(word vram)
+{
+    repeat (2) {
+        repeat (2) {
+            vram = Put(vram, Char_Space);
+        }
+        vram += VramRowSize - 2 * VramStep;
+    }
+    return vram + 2 * VramStep - VramRowSize * 2;
+}
