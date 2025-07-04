222db115ffe901d5d0b32f9392819515205703cc
いぬふと <mxb03523@nifty.com>
2025-07-04 23:14:50 +0900

    Add FMR-30 Cacorm

---
+++ fmr30/cacorm/Sprite.h
@@ -0,0 +1,18 @@
+constexpr byte Sprite_Man = 0;
+constexpr byte Sprite_Monster = 1;
+constexpr byte Sprite_Point = 5;
+constexpr byte Sprite_Increaser = 13;
+constexpr byte SpriteCount = 14;
+
+struct Sprite {
+    byte x, y, pattern;
+    byte oldX, oldY, oldPattern;
+};
+
+extern Sprite[] Sprites;
+extern void InitSprites();
+extern void ShowSprite(ptr<Movable> pMovable, byte pattern);
+extern void HideSprite(byte sprite);
+extern void HideAllSprites();
+extern void EraseSprites();
+extern void DrawSprites();
