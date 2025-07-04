06ac87d9977d8b5c07cae376e88e577e89904cb6
いぬふと <mxb03523@nifty.com>
2025-07-04 22:36:05 +0900

    Add FMR-30 LIFT

---
+++ fmr30/lift/VVram.c
@@ -0,0 +1,45 @@
+#include "VVram.h"
+#include "Chars.h"
+
+byte [VVramWidth * VVramHeight] VVram;
+
+// void VPut2S(ptr<byte> pVVram, ptr<byte> pChars)
+// {
+//     repeat (2) {
+//         repeat (2) {
+//             *pVVram = *pChars;
+//             ++pChars;
+//             ++pVVram;
+//         }
+//         pVVram += VVramWidth - 2;
+//     }
+// }
+
+
+void VPut2C(byte x, byte y, byte c)
+{
+    ptr<byte> pVVram;
+    pVVram = VVramPtr(x, y);
+    repeat (2) {
+        repeat (2) {
+            *pVVram = c;
+            ++c;
+            ++pVVram;
+        }
+        pVVram += VVramWidth - 2;
+    }
+}
+
+
+void VErase2(byte x, byte y)
+{
+    ptr<byte> pVVram;
+    pVVram = VVramPtr(x, y);
+    repeat (2) {
+        repeat (2) {
+            *pVVram = Char_Space;
+            ++pVVram;
+        }
+        pVVram += VVramWidth - 2;
+    }
+}
