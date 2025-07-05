eb02d41e1f20ebb472c1c4376c94e70b0054c835
いぬふと <mxb03523@nifty.com>
2025-07-05 19:41:21 +0900

    Add FMR-30 CAVIT

---
+++ fmr30/cavit/Sprite.c
@@ -0,0 +1,66 @@
+#include "Sprite.h"
+#include "Vram.h"
+#include "VVram.h"
+
+constexpr byte Invalidpattern = 0xff;
+constexpr byte MaxY = VVramHeight * 8 - 16;
+
+struct Sprite {
+    byte x, y, pattern;
+    byte oldX, oldY, oldPattern;
+};
+Sprite[SpriteCount] Sprites;
+
+void HideAllSprites()
+{
+    ptr<Sprite> p;
+    for (p : Sprites) {
+        p->pattern = Invalidpattern;
+        p->oldPattern = Invalidpattern;
+    }
+}
+
+
+void ShowSprite(byte index, byte x, byte y, byte pattern)
+{
+    ptr<Sprite> p;
+    p = Sprites + index;
+    p->x = x;
+    p->y = y;
+    p->pattern = pattern;
+}
+
+
+void HideSprite(byte index) 
+{
+    ptr<Sprite> p;
+    p = Sprites + index;
+    p->pattern = Invalidpattern;
+}
+
+
+void EraseSprites()
+{
+    ptr<Sprite> p;
+    for (p : Sprites) {
+        if (p->oldPattern != Invalidpattern && p->oldY <= MaxY) {
+            EraseBackup(p->oldX, p->oldY);
+        }
+    }
+}
+
+
+void DrawSprites()
+{
+    ptr<Sprite> p;
+    p = Sprites + SpriteCount - 1;
+    repeat (SpriteCount) {
+        if (p->pattern != Invalidpattern && p->y <= MaxY) {
+            DrawSprite(p->x, p->y, p->pattern);
+        }
+        p->oldX = p->x;
+        p->oldY = p->y;
+        p->oldPattern = p->pattern;
+        --p;
+    }
+}
