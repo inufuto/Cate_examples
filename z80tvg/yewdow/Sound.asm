2982ca32b7aed7e96fa9847b812d066f99158ae5
いぬふと <mxb03523@nifty.com>
2025-07-20 06:44:35 +0900

    Add Z80TVGame Yewdow

---
+++ z80tvg/yewdow/Sound.asm
@@ -0,0 +1,276 @@
+Port equ 02h
+Tempo equ 170
+
+dseg
+SoundProc: public SoundProc
+    defw 0
+
+cseg
+InitSound: public InitSound
+    ld hl,0
+    ld (SoundProc),hl
+ret
+
+
+CallSound:
+jp (hl)
+
+CallSound_: public CallSound_
+    push af | push hl
+        ld hl,(SoundProc)
+        ld a,l | or h
+        if nz
+            call CallSound
+            ld hl,0
+            ld (SoundProc),hl
+        endif
+    pop hl | pop af
+ret
+
+
+Tone:
+; in:
+;   bc = cycle
+;   de*a = length
+    do
+        push af | push de
+            do
+                ld a,1
+                out (Port), a
+                push bc
+                    do
+                        dec bc
+                        ld a,c | or b
+                    while nz | wend
+                pop bc
+                ld a,0
+                out (Port), a
+                push bc
+                    do
+                        dec bc
+                        ld a,c | or b
+                    while nz | wend
+                pop bc
+                dec de
+                ld a,e | or d
+            while nz | wend
+        pop de | pop af
+        dec a
+    while nz | wend
+ret 
+
+
+Rest:
+; in:
+;   bc = cycle
+;   de*a = length
+    do
+        push af | push de
+            do
+                ld a,0
+                out (Port), a
+                push bc
+                    do
+                        dec bc
+                        ld a,c | or b
+                    while nz | wend
+                pop bc
+                ld a,0
+                out (Port), a
+                push bc
+                    do
+                        dec bc
+                        ld a,c | or b
+                    while nz | wend
+                pop bc
+                dec de
+                ld a,e | or d
+            while nz | wend
+        pop de | pop af
+        dec a
+    while nz | wend
+ret 
+
+
+Melody: 
+; in
+;   hl: address
+    push af
+    push de
+    push bc
+        do
+            ld c,(hl) | inc hl
+            ld b,(hl) | inc hl
+            ld a,c
+            or a
+        while nz
+            if nz
+                ld a,b
+                or a
+                ld a,c
+                if nz
+                    push hl
+                        dec b
+                        ld l,b | ld h,0
+                        add hl,hl | add hl,hl
+                        ld de,cycles | add hl,de
+                        ld c,(hl) | inc hl
+                        ld b,(hl) | inc hl
+                        ld e,(hl) | inc hl
+                        ld d,(hl)
+                    pop hl
+                    call Tone
+                else
+                    ld bc,(cycles)
+                    ld de,(cycles+2)
+                    call Rest
+                endif
+            endif
+        wend
+    pop bc
+    pop de
+    pop af
+ret
+
+
+loose_notes:
+    defb 1,C4, 0
+Sound_Loose_: public Sound_Loose_
+    push hl
+        ld hl,loose_notes
+        call Melody
+    pop hl
+ret
+
+
+hit_notes:
+    defb 1,F4, 1,G4, 1,A4, 1,B4, 1,C5, 1,D5, 1,E5, 1,F5
+    defb 0 
+Sound_Hit:
+    push hl
+        ld hl,hit_notes
+        call Melody
+    pop hl
+ret
+Sound_Hit_:  public Sound_Hit_
+    push hl
+        ld hl,Sound_Hit
+        ld (SoundProc),hl
+    pop hl
+ret
+
+
+freeze_notes:
+    defb 1,F5, 1,E5, 1,D5, 1,C5, 1,B4, 1,A4, 1,G4, 1,F4
+    defb 0 
+Sound_Freeze:
+    push hl
+        ld hl,freeze_notes
+        call Melody
+    pop hl
+ret
+Sound_Freeze_:  public Sound_Freeze_
+    push hl
+        ld hl,Sound_Freeze
+        ld (SoundProc),hl
+    pop hl
+ret
+
+
+beep_notes:
+    defb 1,A4, 1,0, 0
+Sound_Beep_:  public Sound_Beep_
+    push hl
+        ld hl,beep_notes
+        call Melody
+    pop hl
+ret
+
+
+start_notes: 
+	defb 11,C5,1,0, 11,C5,1,0
+	defb 11,G4,1,0, 11,G4,1,0
+	defb 7,E5,1,0, 3,D5,1,0, 7,E5,1,0, 15,D5,1,0
+	defb 11,D5,1,0, 35-4,C5,1+4,0
+	defb 12,0
+    defb 0
+Sound_Start_: public Sound_Start_
+    push hl
+        ld hl,start_notes
+        call Melody
+    pop hl
+ret
+
+
+clear_notes: 
+	defb 11,C4,1,0, 11,E4,1,0
+	defb 19,G4,1,0, 11,A4,1,0
+	defb 3,A4,1,0, 7,G4,1,0, 3,F4,1,0, 7,G4,1,0
+	defb 3,G4,1,0, 7,A4,1,0, 3,B4,1,0, 35-4,C5,1+4,0
+	defb 12,0
+    defb 0
+Sound_Clear_:
+    public Sound_Clear_
+    push hl
+        ld hl,clear_notes
+        call Melody
+    pop hl
+ret
+
+
+over_notes: 
+	defb 7,C5,1,0, 3,C5,1,0, 7,B4,1,0, 3,B4,1,0
+	defb 7,A4,1,0, 3,A4,1,0, 7,G4,1,0, 3,E4,1,0
+	defb 7,G4,1,0, 3,G4,1,0, 7,A4,1,0, 3,B4,1,0
+	defb 23,C5,1,0
+    defb 0
+Sound_GameOver_: public Sound_GameOver_
+    push hl
+        ld hl,over_notes
+        call Melody
+    pop hl
+ret
+
+
+C4 equ 1
+C4S equ 2
+D4 equ 3
+D4S equ 4
+E4 equ 5
+F4 equ 6
+F4S equ 7
+G4 equ 8
+G4S equ 9
+A4 equ 10
+A4S equ 11
+B4 equ 12
+C5 equ 13
+C5S equ 14
+D5 equ 15
+D5S equ 16
+E5 equ 17
+F5 equ 18
+F5S equ 18
+G5 equ 18
+
+cycles:
+defw 254,1569/Tempo ;C4
+defw 240,1663/Tempo ;C#4
+defw 227,1761/Tempo ;D4
+defw 214,1866/Tempo ;D#4
+defw 202,1977/Tempo ;E4
+defw 190,2095/Tempo ;F4
+defw 180,2219/Tempo ;F#4
+defw 170,2351/Tempo ;G4
+defw 160,2491/Tempo ;G#4
+defw 151,2640/Tempo ;A4
+defw 143,2796/Tempo ;A#4
+defw 134,2963/Tempo ;B4
+defw 127,3139/Tempo ;C5
+defw 120,3326/Tempo ;C#5
+defw 113,3523/Tempo ;D5
+defw 107,3733/Tempo ;D#5
+defw 101,3955/Tempo ;E5
+defw 95,4190/Tempo ;F5
+defw 90,4439/Tempo ;F#5
+defw 85,4703/Tempo ;G5
