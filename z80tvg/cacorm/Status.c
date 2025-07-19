6861ec1392134a153120dad345281c0902fcdd8c
いぬふと <mxb03523@nifty.com>
2025-07-19 13:56:43 +0900

    Add Z80TVGame Cacorm

---
+++ z80tvg/cacorm/Status.c
@@ -0,0 +1,177 @@
+#include "Status.h"
+#include "Vram.h"
+#include "Stage.h"
+#include "Main.h"
+#include "Movable.h"
+#include "Actor.h"
+#include "Sprite.h"
+#include "Print.h"
+#include "Chars.h"
+
+constexpr byte Char_Remain = Char_Man;
+
+static ptr<byte> PrintS(ptr<byte> pVram, ptr<byte> p)
+{
+    byte c;
+    while ((c = *p) != 0) {
+        pVram = PrintC(pVram, c);
+        ++p;
+    }
+    return pVram;
+}
+
+void PrintStatus() 
+{
+    PrintS(Vram + VramRowSize * 1 + 32 * VramStep, "SCORE");
+    PrintS(Vram + VramRowSize * 5 + 32 * VramStep, "HI-SCORE");
+    PrintS(Vram + VramRowSize * 9 + 32 * VramStep, "STAGE");
+    PrintByteNumber2(Vram + VramRowSize * 9 + 38 * VramStep, CurrentStage + 1);
+    PrintS(Vram + VramRowSize * 12 + 32 * VramStep, "TIME");
+
+    {
+        ptr<byte> pVram;
+        pVram = Vram + VramRowSize * 21 + 33 * VramStep;
+        if (RemainCount > 1) {
+            byte i;
+            i = RemainCount - 1;
+            if (i > 2) {
+                Put2C(pVram, Char_Remain);
+                pVram += 2 * VramStep;
+                pVram = Put(pVram, Char_Space);
+                pVram = Put(pVram, Char_Space);
+                pVram += VramRowSize - 2 * VramStep;
+                pVram = Put(pVram, i + 0x10);
+                pVram = Put(pVram, Char_Space);
+            }
+            else {
+                do {
+                    Put2C(pVram, Char_Remain);
+                    pVram += 2 * VramStep;
+                    --i;
+                } while (i > 0);
+            }
+        }
+    }
+
+    PrintScore();
+    PrintTime();
+}
+
+void PrintScore()
+{
+    ptr<byte> pVram;
+    
+    pVram = PrintNumber5(Vram + VramRowSize * 2 + 34 * VramStep, Score);
+    PrintC(pVram, '0');
+
+    pVram = PrintNumber5(Vram + VramRowSize * 6 + 34 * VramStep, HiScore);
+    PrintC(pVram, '0');
+}
+
+void PrintTime()
+{
+    PrintByteNumber3(Vram + VramRowSize * 12 + 37 * VramStep, StageTime);
+}
+
+static void EraseWindow() 
+{
+    constexpr byte width = 12;
+    constexpr byte height = 3;    
+    ptr<byte> pVram;
+
+    pVram = Vram + VramRowSize * 10 + 10 * VramStep;
+    repeat (height) {
+        repeat (width) {
+            pVram = PrintC(pVram, ' ');
+        }
+        pVram += VramRowSize - width * VramStep;
+    }
+}
+
+void PrintGameOver()
+{
+    EraseWindow();
+    PrintS(Vram + VramRowSize * 11 + 12 * VramStep, "GAME OVER");
+    PresentVram();
+}
+
+void PrintTimeUp() 
+{
+    EraseWindow();
+    PrintS(Vram + VramRowSize * 11 + 13 * VramStep, "TIME UP");
+    PresentVram();
+}
+
+void Title()
+{
+    static const byte[] TitleBytes = {
+		0x48, 0x47, 0x4d, 0x42, 
+		0x4f, 0x40, 0x40, 0x40, 
+		0x4d, 0x42, 0x48, 0x42, 
+		0x40, 0x45, 0x45, 0x40, 
+		0x40, 0x40, 0x40, 0x40, 
+		0x44, 0x4d, 0x42, 0x4e, 
+		0x4e, 0x4d, 0x43, 0x4f, 
+		0x44, 0x45, 0x41, 0x44, 
+		0x40, 0x40, 0x40, 0x40, 
+		0x4d, 0x42, 0x4e, 0x4d, 
+		0x48, 0x42, 0x4f, 0x4c, 
+		0x45, 0x40, 0x44, 0x45, 
+		0x40, 0x40, 0x40, 0x40, 
+		0x42, 0x4f, 0x46, 0x41, 
+		0x43, 0x4f, 0x40, 0x40, 
+		0x40, 0x45, 0x40, 0x40, 
+		0x40, 0x40, 0x40, 0x40, 
+		0x4f, 0x4d, 0x4d, 0x42, 
+		0x4f, 0x4c, 0x4c, 0x43, 
+		0x45, 0x44, 0x44, 0x41, 
+    };
+
+    ClearScreen(); 
+    HideAllSprites();
+    PrintStatus();
+    {
+        ptr<byte> p;
+        ptr<byte> pVram;
+        pVram = Vram + VramRowSize * 8 + 6 * VramStep;
+        p = TitleBytes;
+        repeat (5) {
+            repeat (4) {
+                repeat (4) {
+                    pVram = Put(pVram, *p);
+                    ++p;
+                }
+                pVram += VramRowSize - 4 * VramStep;
+            }
+            pVram += 4 * VramStep - VramRowSize * 4;
+        }
+    }
+    PrintS(Vram + VramRowSize * 20 + 6 * VramStep, "PUSH TRIGGER BUTTON");
+    PrintS(Vram + VramRowSize * 22 + 20 * VramStep, "INUFUTO 2025");
+    // {
+    //     byte c;
+    //     ptr<byte> pVram;
+    //     pVram = Vram;
+    //     c = 0;
+    //     repeat (Char_End) {
+    //         pVram = Put(pVram, c);
+    //         ++c;
+    //         if ((c & 15) == 0) {
+    //             pVram += VramRowSize - 16 * VramStep;
+    //         }
+    //     }
+    // }
+    PresentVram();
+}
+
+
+void DrawFence()
+{
+    ptr<byte> pTop = Vram;
+    ptr<byte> pBottom = Vram + VramRowSize * 23;
+    repeat(StageWidth) {
+        pTop = Put(pTop, Char_Fence);
+        pBottom = Put(pBottom, Char_Fence + 1);
+    }
+    PresentVram();
+}
