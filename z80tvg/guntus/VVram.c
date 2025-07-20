7042efa353558602a6af5a997adf6707e805bf6a
いぬふと <mxb03523@nifty.com>
2025-07-20 18:11:34 +0900

    Add Z80TVGame GUNTUS

---
+++ z80tvg/guntus/VVram.c
@@ -0,0 +1,41 @@
+#include "Vram.h"
+#include "VVram.h"
+#include "Chars.h"
+#include "Sprite.h"
+#include "EnemyRow.h"
+#include "Star.h"
+#include "Sound.h"
+
+bool BackgroundChanged;
+
+void DrawAll() 
+{
+    if (BackgroundChanged) {
+        BackgroundChanged = false;
+        ClearVVram();
+        DrawStars();
+        DrawEnemyRows();
+        VVramBackToFront();
+        DrawSprites();
+    }
+    else {
+        EraseSprites();
+        DrawSprites();
+    }
+    VVramToVram();
+    PresentVram();
+    CallSound();
+}
+
+
+ptr<byte> VPut2C(ptr<byte> pVVram, byte c)
+{
+    repeat (2) {
+        repeat (2) {
+            *pVVram = c; ++pVVram;
+            ++c;
+        }
+        pVVram += VVramWidth - 2;
+    }
+    return pVVram + 2 - VVramWidth * 2;
+}
