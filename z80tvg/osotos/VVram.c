aee4ef1a442a27421d7882eabd8f3d76b87fe546
いぬふと <mxb03523@nifty.com>
2025-07-20 09:05:37 +0900

    Add Z80TVGame OSOTOS

---
+++ z80tvg/osotos/VVram.c
@@ -0,0 +1,16 @@
+#include "VVram.h"
+#include "Block.h"
+#include "Sprite.h"
+#include "Vram.h"
+#include "Sound.h"
+
+byte[VVramWidth * VVramHeight] VVram;
+
+void DrawAll() {    
+    DrawBackground();
+    DrawBlocks();
+    DrawSprites();
+    VVramToVram();
+    PresentVram();
+    CallSound();
+}
