481a2eb6b300db93ca11c8f6c51629add233a0da
いぬふと <mxb03523@nifty.com>
2025-07-21 09:39:24 +0900

    Add Z80TVGame IMPETUS

---
+++ z80tvg/impetus/Chars.h
@@ -0,0 +1,16 @@
+
+constexpr byte Char_Space = 0x0;
+constexpr byte Char_Ascii = 0x0;
+constexpr byte Char_Logo = 0x40;
+constexpr byte Char_FighterBullet = 0x50;
+constexpr byte Char_EnemyBullet = 0x51;
+constexpr byte Char_Barrier = 0x52;
+constexpr byte Char_BarrierHead = 0x53;
+constexpr byte Char_Fighter = 0x54;
+constexpr byte Char_SkyEnemy = 0x58;
+constexpr byte Char_GroundEnemy = 0x80;
+constexpr byte Char_Bang = 0x88;
+constexpr byte Char_Item = 0x9C;
+constexpr byte Char_Fort = 0xA0;
+constexpr byte Char_Terrain = 0xC4;
+constexpr byte Char_End = 0xCE;
---
+++ z80tvg/impetus/VVram.c
@@ -0,0 +1,32 @@
+#include "VVram.h"
+#include "Chars.h"
+#include "Sprite.h"
+#include "Vram.h"
+#include "Fort.h"
+#include "Barrier.h"
+#include "Sound.h"
+
+bool VVramChanged;
+
+
+void DrawAll()
+{
+    if (VVramChanged) {
+        VVramChanged = false;
+        VVramBackToFront();
+        DrawFort();
+        DrawBarriers();
+        DrawSprites();
+    }
+    else {
+        EraseSprites();
+        EraseBarriers();
+        EraseFort();
+        DrawFort();
+        DrawBarriers();
+        DrawSprites();
+    }
+    VVramToVram();
+    PresentVram();
+    CallSound();
+}
