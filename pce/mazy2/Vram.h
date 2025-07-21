0bd64ac1d1b75a3671457edf4e9b8a98a6953acd
いぬふと <mxb03523@nifty.com>
2025-07-21 23:45:25 +0900

    Add "rom.csx" for PCEngine

--- pce/mazy2/Vram.h
+++ pce/mazy2/Vram.h
@@ -11,9 +11,6 @@ constexpr byte StatusHeight = 2;
 extern void ClearScreen();
 extern word VramAddress(byte x, byte y);
 extern word Put(word vram, byte c);
-extern byte ReadVram(word vram);
 
 extern word PrintC(word vram, byte c);
 extern word Put2C(word vram, byte c);
-extern word Erase2(word vram);
-extern word Put2S(word vram, ptr<byte> pChars);
