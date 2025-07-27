b189bbeed2346bb73b53928556fcd3867c014f2b
いぬふと <mxb03523@nifty.com>
2025-07-27 22:21:50 +0900

    Add PASOPIA7 Cacorm

---
+++ pasopia7/cacorm/Vram.c
@@ -0,0 +1,37 @@
+#include "Vram.h"
+#include "Chars.h"
+
+extern byte[] MonoChars, ColorChars, Ascii, FenceChars;
+extern byte[] LineChars;
+
+
+void InitVram()
+{
+    ClearScreen();
+    MakePatternMono(Char_Ascii, Ascii, 0x40, 7);
+    MakePatternMono(Char_Logo, MonoChars + (Char_Logo - Char_Logo) * CharHeight, 16, 5);
+    MakePatternMono(Char_Block, MonoChars + (Char_Block - Char_Logo) * CharHeight, 4, 1);
+    MakePatternMono(Char_Item, MonoChars + (Char_Item - Char_Logo) * CharHeight, 4, 6);
+    MakePatternMono(Char_Line_Normal, LineChars, 8, 7);
+    MakePatternMono(Char_Line_Red, LineChars, 8, 2);
+    MakePatternMono(Char_Fence, FenceChars, 2, 1);
+    MakePatternColor(Char_Man, ColorChars, Char_Fence - Char_Man);
+}
+
+
+word PrintC(word vram, byte c)
+{
+    return Put(vram, c - ' ');
+}
+
+
+void Put2C(word vram, byte c)
+{
+    repeat (2) {
+        repeat (2) {
+            vram = Put(vram, c);
+            ++c;
+        }
+        vram += VramRowSize - 2 * VramStep;
+    }
+}
---
+++ pasopia7/cacorm/Vram.h
@@ -0,0 +1,20 @@
+constexpr word Vram = 0x8000;
+
+constexpr byte VramWidth = 40;
+constexpr byte VramHeight = 25;
+constexpr byte CharHeight = 8;
+constexpr byte VramStep = CharHeight;
+constexpr byte VramRowSize = VramWidth * VramStep;
+
+constexpr byte PatternSize = CharHeight * 3;
+constexpr ptr<byte> PatternRam = (ptr<byte>)0xd000;
+
+extern void MakePatternMono(byte c, ptr<byte> pSource, byte count, byte color);
+extern void MakePatternColor(byte c, ptr<byte> pSource, byte count);
+extern void ClearScreen();
+extern word Put(word vram, byte c);
+extern void VVramToVram();
+
+extern void InitVram();
+extern word PrintC(word vram, byte c);
+extern void Put2C(word vram, byte c);
