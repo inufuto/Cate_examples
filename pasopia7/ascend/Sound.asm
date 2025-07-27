5ad0c93c902ce875c36104bb97270e130b7cf4e9
いぬふと <mxb03523@nifty.com>
2025-07-27 22:08:35 +0900

    Add PASOPIA7 ASCEND

---
+++ pasopia7/ascend/Sound.asm
@@ -0,0 +1,395 @@
+Port_Dcsg equ 03ah
+
+Dcsg_ToneA equ 00h
+Dcsg_VolumeA equ 10h
+Dcsg_ToneB equ 20h
+Dcsg_VolumeB equ 30h
+Dcsg_ToneC equ 40h
+Dcsg_VolumeC equ 50h
+Dcsg_NoiseControl equ 60h
+Dcsg_NoiseVolume equ 70h
+
+MinVolume equ 63
+
+tempo equ 160
+
+count equ 3
+Channel_Ptr equ 0
+Channel_Start equ Channel_Ptr+2
+Channel_Len equ Channel_Start+2
+Channel_Vol equ Channel_Len+1
+unitSize equ Channel_Vol+1
+
+    dseg
+Channels defs unitSize*count
+time: defw 0
+
+
+N8 equ 6
+N8P equ N8*3/2
+N4 equ N8*2
+N4P equ N4*3/2
+N2 equ N4*2
+N2P equ N2*3/2
+N1 equ N2*2
+
+
+cseg
+
+InitSound_: public InitSound_
+    di
+        ld ix,Channels
+        ld b,count
+        ld de,unitSize
+        xor a
+        do
+            ld (ix+Channel_Ptr),a
+            ld (ix+Channel_Ptr+1),a
+            ld (ix+Channel_Len),1
+            add ix,de
+        dwnz
+
+        ld hl,0
+        ld (time),hl
+    ei
+ret
+
+Tone:
+    ld a,l
+    and 0fh
+    or 80h or Dcsg_ToneA
+    out (Port_Dcsg),a
+    add hl,hl
+    add hl,hl
+    add hl,hl
+    add hl,hl
+    ld a,h
+    and 3fh
+    out (Port_Dcsg),a
+ret
+
+
+SoundHandler: public SoundHandler
+    push hl | push de
+        ld hl,(time)
+        ld de,-tempo
+        add hl,de
+        if nc
+            push hl | push bc | push ix
+                ld ix,Channels
+                ld bc,(count * 100h) or Dcsg_ToneA   ; b = count; c = register
+                do
+                    ld l,(ix+Channel_Ptr)
+                    ld h,(ix+Channel_Ptr+1)
+                    ld a,l
+                    or h
+                    if nz
+                        dec (ix+Channel_Len)
+                        if z
+                            continue:
+                            ld a,(hl)
+                            or a
+                            if z
+                                ; end
+                                ld (ix+Channel_Ptr),a
+                                ld (ix+Channel_Ptr+1),a
+                                ld a,c
+                                or 9fh
+                                out (Port_Dcsg),a
+                                jr next
+                            endif
+                            inc a
+                            if z 
+                                ; repeat
+                                ld l,(ix+Channel_Start)
+                                ld h,(ix+Channel_Start+1)
+                                jr continue
+                            endif
+                            dec a
+                            ld (ix+Channel_Len),a
+                            inc hl
+                            ld a,(hl)
+                            inc hl
+                            ld (ix+Channel_Ptr),l
+                            ld (ix+Channel_Ptr+1),h
+                            or a
+                            if nz
+                                dec a
+                                add a,a
+                                ld e,a
+                                ld d,0
+                                ld hl,cycles
+                                add hl,de
+                                ld a,c
+                                or 80h
+                                or (hl)
+                                out (Port_Dcsg),a
+                                inc hl
+                                ld a,(hl)
+                                out (Port_Dcsg),a
+                                ld a,c
+                                cp Dcsg_ToneC
+                                if z 
+                                    ld a,4
+                                else
+                                    xor a
+                                endif
+                                ld (ix+Channel_Vol),a
+                            else
+                                ld (ix+Channel_Vol),MinVolume
+                            endif
+                            EndWriteCycle:
+                        endif
+                        ld a,(ix+Channel_Vol)
+                        srl a | srl a
+                        or c
+                        or 90h
+                        out (Port_Dcsg),a
+                        ld a,(ix+Channel_Vol)
+                        cp MinVolume
+                        if nz
+                            inc a
+                            ld (ix+Channel_Vol),a
+                        endif
+                    endif
+                    next:
+                    ld a,c
+                    add a,20h
+                    ld c,a
+                    ld de,unitSize
+                    add ix,de
+                dwnz
+            pop ix | pop bc | pop hl
+            ld de,640/2
+            add hl,de
+        endif
+        ld (time),hl
+    pop de | pop hl
+ret
+
+Melody:
+    di
+    ld (Channels+Channel_Ptr),hl
+    ld a,1
+    ld (Channels+Channel_Len),a
+    ei
+ret
+
+MelodyWait:
+    call Melody
+    do
+        ld hl,(Channels+Channel_Ptr)
+        ld a,l
+        or h
+    while nz | wend
+ret 
+
+
+loose_notes:
+    defb 1,A3, 0
+Sound_Loose_: public Sound_Loose_
+    push af | push hl
+        ld hl,loose_notes
+        call MelodyWait
+    pop hl | pop af
+ret
+
+
+item_notes:
+    defb 1,C4, 1, C4S, 1,D4, 1,F4, 1,A4, 1,C5, 0
+Sound_Item_: public Sound_Item_
+    push af | push hl
+        ld hl,item_notes
+        call Melody
+    pop hl | pop af
+ret
+
+
+beep_notes:
+    defb 1,A4, 0
+Sound_Beep_: public Sound_Beep_
+    push af | push hl
+        ld hl,beep_notes
+        call MelodyWait
+    pop hl | pop af
+ret
+
+
+start_notes: 
+    defb N8,0, N8,C5, N8,C5, N8,C5, N8,C5, N4,G4, N4,C5
+    defb N8,C5, N8,D5, N8,C5, N4,D5, N4,E5
+    defb N1,C5
+    defb 0
+Sound_Start_: public Sound_Start_
+    push af | push hl
+        ld hl,start_notes
+        call MelodyWait
+    pop hl | pop af
+ret
+
+
+clear_notes: 
+    defb N8,C4, N8,E4, N8,G4, N8,D4, N8,F4, N8,A4, N8,E4, N8,G4, N8,B4, N4P,C5,0       
+    defb 0
+Sound_Clear_: 
+    public Sound_Clear_
+    push af | push hl
+        ld hl,clear_notes
+        call MelodyWait
+    pop hl | pop af
+ret
+
+
+over_notes: 
+    defb N8,C5, N8,C5, N8,G4, N8,G4, N8,A4, N8,A4, N8,B4, N8,B4
+    defb N2P,C5, N4,0
+    defb 0
+Sound_GameOver_: 
+    public Sound_GameOver_
+    push af | push hl
+        ld hl,over_notes
+        call MelodyWait
+    pop hl | pop af
+ret
+
+
+BGM_B:
+    defb N8,0, N8,C5, N8,C5, N8,C5, N8,C5, N4,G4, N4,C5
+    defb N8,C5, N8,D5, N8,C5, N4,D5, N4,E5
+    defb N8,0, N8,C5, N8,C5, N8,C5, N8,C5, N4,D5, N4,F5
+    defb N8,F5, N8,E5, N8,C5, N4,C5, N4,D5
+    defb N8,0, N8,C5, N8,C5, N8,C5, N8,C5, N4,G4, N4,C5
+    defb N8,C5, N8,D5, N8,C5, N4,D5, N4,E5
+    defb N4,F5, N4,F5, N4,E5, N4,E5
+    defb N4,D5, N8,D5, N4,E5, N8,E5, N8,D5, N8,C5
+    defb N4,C5, N4,C5, N8,D5, N4,D5, N4P,C5, N2P,0
+    defb 0ffh
+BGM_C:
+    defb N8,C3, N4,0, N4P,E3, N8,G3, N8,0 ; 3
+    defb N8,A2, N4,0, N4P,C3, N8,E3, N8,0 ; 4
+    defb N8,D3, N4,0, N4P,F3, N8,A3, N8,0 ; 5
+    defb N8,G2, N4,0, N4P,B2, N8,D3, N8,0 ; 6
+    defb N8,C3, N4,0, N4P,E3, N8,G3, N8,0 ; 7
+    defb N8,A2, N4,0, N4P,C3, N8,E3, N8,0 ; 8
+    defb N8,F2, N4,0, N4P,A2, N8,C3, N8,0 ; 9
+    defb N8,D3, N4,0, N4P,E3, N8,A3, N8,0 ; 10
+    defb N8,F2, N4,0, N8,F2, N8,G2, N8,0, N8,B2, N8,D3 ; 11
+    defb N8,C3, N4,0, N4P,E3, N8,G3, N8,0 ; 12
+    defb 0ffh
+
+StartBGM_: public StartBGM_
+    push af | push hl
+        di
+        ld a,1
+        ld hl,BGM_B
+        ld (Channels+unitSize*1+Channel_Start),hl
+        ld (Channels+unitSize*1+Channel_Ptr),hl
+        ld (Channels+unitSize*1+Channel_Len),a
+        ld hl,BGM_C
+        ld (Channels+unitSize*2+Channel_Start),hl
+        ld (Channels+unitSize*2+Channel_Ptr),hl
+        ld (Channels+unitSize*2+Channel_Len),a
+        ei
+    pop hl | pop af
+ret
+
+StopBGM_: public StopBGM_
+    push af | push hl | push bc
+        di
+        ld hl,0
+        ld (Channels+unitSize*1+Channel_Ptr),hl
+        ld (Channels+unitSize*2+Channel_Ptr),hl
+        ld b,3
+        ld a, Dcsg_VolumeB or 8fh
+        do
+            out (Port_Dcsg),a
+            add a,20h
+        dwnz
+        ei
+    pop bc | pop hl | pop af
+ret
+
+
+E2 equ 1
+F2 equ 2
+F2S equ 3
+G2 equ 4
+G2S equ 5
+A2 equ 6
+A2S equ 7
+B2 equ 8
+C3 equ 9
+C3S equ 10
+D3 equ 11
+D3S equ 12
+E3 equ 13
+F3 equ 14
+F3S equ 15
+G3 equ 16
+G3S equ 17
+A3 equ 18
+A3S equ 19
+B3 equ 20
+C4 equ 21
+C4S equ 22
+D4 equ 23
+D4S equ 24
+E4 equ 25
+F4 equ 26
+F4S equ 27
+G4 equ 28
+G4S equ 29
+A4 equ 30
+A4S equ 31
+B4 equ 32
+C5 equ 33
+C5S equ 34
+D5 equ 35
+D5S equ 36
+E5 equ 37
+F5 equ 38
+F5S equ 39
+G5 equ 40
+
+
+cycles:
+defb 5,47 ; E2
+defb 10,44 ; F2
+defb 2,42 ; F#2
+defb 12,39 ; G2
+defb 9,37 ; G#2
+defb 7,35 ; A2
+defb 7,33 ; A#2
+defb 9,31 ; B2
+defb 13,29 ; C3
+defb 2,28 ; C#3
+defb 8,26 ; D3
+defb 1,25 ; D#3
+defb 10,23 ; E3
+defb 5,22 ; F3
+defb 1,21 ; F#3
+defb 14,19 ; G3
+defb 12,18 ; G#3
+defb 11,17 ; A3
+defb 11,16 ; A#3
+defb 12,15 ; B3
+defb 14,14 ; C4
+defb 1,14 ; C#4
+defb 4,13 ; D4
+defb 8,12 ; D#4
+defb 13,11 ; E4
+defb 2,11 ; F4
+defb 8,10 ; F#4
+defb 15,9 ; G4
+defb 6,9 ; G#4
+defb 13,8 ; A4
+defb 5,8 ; A#4
+defb 14,7 ; B4
+defb 7,7 ; C5
+defb 0,7 ; C#5
+defb 10,6 ; D5
+defb 4,6 ; D#5
+defb 14,5 ; E5
+defb 9,5 ; F5
+defb 4,5 ; F#5
+defb 15,4 ; G5
---
+++ pasopia7/ascend/Vram.c
@@ -0,0 +1,32 @@
+#include "Vram.h"
+#include "Chars.h"
+
+extern byte[] MonoChars, ColorChars, Ascii;
+
+
+void InitVram()
+{
+    ClearScreen();
+    MakePatternMono(Char_Ascii, Ascii, 0x40, 7);
+    MakePatternMono(Char_Logo, MonoChars + (Char_Logo - Char_Logo) * CharHeight, 16 + 2, 6);
+    MakePatternMono(Char_Floor, MonoChars + (Char_Floor - Char_Logo) * CharHeight, 2, 1);
+    MakePatternColor(Char_Man, ColorChars, Char_End - Char_Man);
+}
+
+
+word PrintC(word vram, byte c)
+{
+    return Put(vram, c - ' ');
+}
+
+
+void Put2C(word vram, byte c)
+{
+    repeat (2) {
+        repeat (2) {
+            vram = Put(vram, c);
+            ++c;
+        }
+        vram += VramRowSize - 2 * VramStep;
+    }
+}
