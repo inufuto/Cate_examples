63b63f4117264d9a7a3b614ee183bd8e6ec44d69
いぬふと <mxb03523@nifty.com>
2025-07-28 21:03:54 +0900

    Add PASOPIA7 OSOTOS

---
+++ pasopia7/osotos/Stages.h
@@ -0,0 +1,7 @@
+#include "Stage.h"
+
+constexpr byte StageCount = 8;
+constexpr byte MaxBlockCount = 10;
+constexpr byte MaxMonsterCount = 4;
+
+extern const Stage[] Stages;
---
+++ pasopia7/osotos/VVram.h
@@ -0,0 +1,9 @@
+constexpr byte VVramWidth = 32;
+constexpr byte VVramHeight = 25;
+
+extern byte[] VVram;
+
+extern ptr<byte> VVramPtr(byte x, byte y);
+extern void DrawBackground();
+
+extern void DrawAll();
