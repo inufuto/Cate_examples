32a63978555063d57eb173ac1228ab144ef3c504
いぬふと <mxb03523@nifty.com>
2025-10-15 15:29:16 +0900

    Add MAP-1010 Bootskell

---
+++ map1010/bootskell/Chars.h
@@ -0,0 +1,8 @@
+constexpr byte Char_Space = 0x0;
+constexpr byte Char_Logo = 0x0;
+constexpr byte Char_Wall = 0x10;
+constexpr byte Char_Block = 0x14;
+constexpr byte Char_Man = 0x38;
+constexpr byte Char_Monster = 0x58;
+constexpr byte Char_Point = 0x78;
+constexpr byte Char_End = 0x88;
---
+++ map1010/bootskell/Vram.c
@@ -0,0 +1,5 @@
+#include "Vram.h"
+
+void InitVram()
+{
+}
