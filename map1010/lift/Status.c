d249caabf2ded850dd768a14c77d667118020fc6
いぬふと <mxb03523@nifty.com>
2025-10-15 12:32:52 +0900

    Add MAP-1010 LIFT

---
+++ map1010/lift/Status.c
@@ -0,0 +1,144 @@
+#include "Movable.h"
+#include "Status.h"
+#include "Vram.h"
+#include "Stage.h"
+#include "Main.h"
+#include "Sprite.h"
+#include "Print.h"
+#include "Chars.h"
+
+static ptr<byte> PrintS(ptr<byte> pVram, ptr<byte> p)
+{
+    byte c;
+    while ((c = *p) != 0) {
+        pVram = PrintC(pVram, c);
+        ++p;
+    }
+    return pVram;
+}
+
+void PrintStatus() 
+{
+    PrintS(Vram + 1, "SCORE");
+    PrintS(Vram + 7, "HI-SCORE");
+    PrintS(Vram + 16, "STAGE");
+    PrintByteNumber2(Vram + VramRowSize + 18, CurrentStage + 1);
+    PrintS(Vram+ 22, "TIME");
+
+    {
+        ptr<byte> pVram;
+        pVram = Vram + 27;
+        if (RemainCount > 1) {
+            byte i;
+            i = RemainCount - 1;
+            do {
+                byte c;
+                c = Char_Man_Left_Stop;
+                repeat (2) {
+                    repeat (2) {
+                        pVram = Put(pVram, c);
+                        ++c;
+                    }
+                    pVram += VramRowSize - 2;
+                }
+                pVram += 2 - VramRowSize * 2;
+                --i;
+            } while (i > 0);
+        }
+    }
+
+    PrintScore();
+    PrintTime();
+}
+
+void PrintScore()
+{
+    ptr<byte> pVram;
+    
+    pVram = PrintNumber5(Vram + VramRowSize, Score);
+    PrintC(pVram, '0');
+
+    pVram = PrintNumber5(Vram + VramRowSize + 9, HiScore);
+    PrintC(pVram, '0');
+}
+
+void PrintTime()
+{
+    PrintByteNumber3(Vram + VramRowSize + 23, StageTime);
+}
+
+static void EraseWindow() 
+{
+    constexpr byte width = 12;
+    constexpr byte height = 3;    
+    ptr<byte> pVram;
+
+    pVram = Vram + VramRowSize * 11 + 10;
+    repeat (height) {
+        repeat (width) {
+            pVram = PrintC(pVram, ' ');
+        }
+        pVram += VramRowSize - width;
+    }
+}
+
+void PrintGameOver()
+{
+    EraseWindow();
+    PrintS(Vram + VramRowSize * 12 + 11, "GAME OVER");
+}
+
+void PrintTimeUp() 
+{
+    EraseWindow();
+    PrintS(Vram + VramRowSize * 12 + 12, "TIME UP");
+}
+
+void Title()
+{
+    static const byte[] TitleBytes = {
+        //	L
+        0x0c, 0x03, 0x00, 0x00, 
+        0x0c, 0x03, 0x00, 0x00, 
+        0x0c, 0x03, 0x00, 0x00, 
+        0x04, 0x05, 0x05, 0x01, 
+        //	I
+        0x04, 0x0d, 0x07, 0x01, 
+        0x00, 0x0c, 0x03, 0x00, 
+        0x00, 0x0c, 0x03, 0x00, 
+        0x04, 0x05, 0x05, 0x01, 
+        //	F
+        0x0c, 0x07, 0x05, 0x01, 
+        0x0c, 0x0b, 0x0a, 0x02, 
+        0x0c, 0x03, 0x00, 0x00, 
+        0x04, 0x01, 0x00, 0x00, 
+        //	T
+        0x04, 0x0d, 0x07, 0x01, 
+        0x00, 0x0c, 0x03, 0x00, 
+        0x00, 0x0c, 0x03, 0x00, 
+        0x00, 0x04, 0x01, 0x00, 
+   };
+
+    ClearScreen(); 
+    HideAllSprites();
+    PrintStatus();
+    {
+        ptr<byte> p;
+        ptr<byte> pVram;
+        pVram = Vram + VramRowSize * 7 + (32 - 4 * 4) / 2;
+        p = TitleBytes;
+        repeat (4) {
+            repeat (4) {
+                repeat (4) {
+                    pVram = Put(pVram, *p);
+                    ++p;
+                }
+                pVram += VramRowSize - 4;
+            }
+            pVram += 4 - VramRowSize * 4;
+        }
+    }
+    PrintS(Vram + VramRowSize * 19 + 9, "PUSH SPACE KEY");
+    PrintS(Vram + VramRowSize * 20 + 9, "OR SHIFT KEY");
+    PrintS(Vram + VramRowSize * 23 + 20, "INUFUTO 2022");
+}
---
+++ map1010/lift/Vram.h
@@ -0,0 +1,13 @@
+constexpr byte VramWidth = 32;
+constexpr byte VramHeight = 24;
+constexpr byte CharHeight = 8;
+constexpr word VramRowSize = VramWidth * CharHeight;
+
+constexpr ptr<byte> Vram = (ptr<byte>)0x6000;
+
+extern void ClearScreen();
+extern ptr<byte>Put(ptr<byte> pVram, byte c);
+extern void VVramToVram();
+extern ptr<byte> PrintC(ptr<byte> pVram, byte c);
+
+extern void Put2C(ptr<byte> pVram, byte c);
