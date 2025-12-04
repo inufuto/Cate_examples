af1c1f613ebe31413ad560a4dc915fbee8d2822a
いぬふと <mxb03523@nifty.com>
2025-12-04 20:19:04 +0900

    Add FMR-50 IMPETUS+

---
+++ fmr50/impetusp/Movable.h
@@ -0,0 +1,9 @@
+constexpr byte CoordShift = 1;
+constexpr byte CoordRate = 1 << CoordShift;
+constexpr byte CoordMask = CoordRate - 1;
+
+struct Movable
+{
+    byte x, y;
+    byte sprite;
+};
---
+++ fmr50/impetusp/Vram.h
@@ -0,0 +1,18 @@
+constexpr word Vram = 0x0000;
+
+constexpr byte VramWidth = 80;
+constexpr byte VramHeight = 25;
+constexpr byte CharHeight = 8;
+constexpr byte VramStep = VramWidth / 40;
+constexpr byte VramRowSize = VramWidth*CharHeight;
+
+extern void ClearScreen();
+extern word Put(word vram, byte c);
+extern word PrintC(word vram, byte c);
+extern void VVramToVram();
+extern void SwitchVram();
+extern void EraseBackup(byte x, byte y);
+extern void DrawSprite1(byte x, byte y, byte pattern);
+extern void DrawSprite2(byte x, byte y, byte pattern);
+
+extern void DrawAll();
