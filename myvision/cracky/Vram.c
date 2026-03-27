2f59165dd50b1f29f7606ff1ab38c6a64f063fbb
Inufuto <mxb03523@nifty.com>
2026-03-28 07:19:14 +0900

    Add My Vision CRACKY

---
+++ myvision/cracky/Vram.c
@@ -0,0 +1,13 @@
+#include "Vram.h"
+
+word Put2C(word vram, byte c)
+{
+    repeat (2) {
+        repeat (2) {
+            vram = Put(vram, c);
+            ++c;
+        }
+        vram += VramWidth - 2;
+    }
+    return vram + VramRowSize - 2;
+}
