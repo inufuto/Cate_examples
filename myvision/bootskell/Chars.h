57cbdbe974a28b1ecb1de5367d8a69c8fda65445
Inufuto <mxb03523@nifty.com>
2026-03-29 07:33:44 +0900

    Add My Vision Bootskell

---
+++ myvision/bootskell/Chars.h
@@ -0,0 +1,7 @@
+constexpr byte Char_Space = 0x0;
+constexpr byte Char_Ascii = 0x0;
+constexpr byte Char_Logo = 0x40;
+constexpr byte Char_Remain = 0x50;
+constexpr byte Char_Wall = 0x54;
+constexpr byte Char_Block = 0x58;
+constexpr byte Char_End = 0x7C;
