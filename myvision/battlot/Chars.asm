11c6b61a969662defbdd90fcfc162335b4b33287
Inufuto <mxb03523@nifty.com>
2026-03-29 15:41:14 +0900

    Add My Vision BATTLOT

---
+++ myvision/battlot/Chars.asm
@@ -0,0 +1,211 @@
+cseg
+
+Chars: public Chars
+    ; Ascii
+	defb 000H,000H,000H,000H,000H,000H,000H,000H
+	defb 018H,018H,018H,018H,018H,000H,018H,000H
+	defb 036H,036H,036H,000H,000H,000H,000H,000H
+	defb 006H,037H,03eH,077H,03eH,076H,030H,000H
+	defb 008H,03fH,048H,03eH,009H,07eH,008H,000H
+	defb 000H,032H,054H,068H,016H,02aH,04cH,000H
+	defb 038H,06cH,06cH,038H,06bH,066H,03bH,000H
+	defb 00cH,018H,030H,000H,000H,000H,000H,000H
+	defb 00cH,018H,030H,030H,030H,018H,00cH,000H
+	defb 030H,018H,00cH,00cH,00cH,018H,030H,000H
+	defb 008H,02aH,01cH,07fH,01cH,02aH,008H,000H
+	defb 000H,018H,018H,07eH,018H,018H,000H,000H
+	defb 000H,000H,000H,000H,018H,018H,030H,000H
+	defb 000H,000H,000H,07eH,000H,000H,000H,000H
+	defb 000H,000H,000H,000H,000H,018H,018H,000H
+	defb 000H,003H,006H,00cH,018H,030H,060H,000H
+	defb 03eH,063H,067H,06bH,073H,063H,03eH,000H
+	defb 018H,078H,018H,018H,018H,018H,07eH,000H
+	defb 03eH,063H,007H,01eH,03cH,070H,07fH,000H
+	defb 03eH,063H,003H,01eH,003H,063H,03eH,000H
+	defb 006H,00eH,01eH,036H,066H,07fH,006H,000H
+	defb 07fH,060H,07eH,003H,003H,063H,03eH,000H
+	defb 01eH,030H,060H,07eH,063H,063H,03eH,000H
+	defb 07fH,063H,006H,00cH,018H,018H,018H,000H
+	defb 03eH,063H,063H,03eH,063H,063H,03eH,000H
+	defb 03eH,063H,063H,03fH,003H,063H,03eH,000H
+	defb 000H,000H,018H,000H,000H,018H,000H,000H
+	defb 000H,000H,018H,000H,000H,018H,030H,000H
+	defb 00cH,018H,030H,060H,030H,018H,00cH,000H
+	defb 000H,000H,07eH,000H,07eH,000H,000H,000H
+	defb 030H,018H,00cH,006H,00cH,018H,030H,000H
+	defb 03eH,063H,003H,00eH,018H,000H,018H,000H
+	defb 01eH,021H,04dH,055H,04eH,020H,01eH,000H
+	defb 01cH,036H,063H,063H,07fH,063H,063H,000H
+	defb 07eH,063H,063H,07eH,063H,063H,07eH,000H
+	defb 01eH,033H,060H,060H,060H,033H,01eH,000H
+	defb 07cH,066H,063H,063H,063H,066H,07cH,000H
+	defb 07eH,060H,060H,07eH,060H,060H,07eH,000H
+	defb 07eH,060H,060H,07eH,060H,060H,060H,000H
+	defb 01eH,033H,060H,06fH,063H,033H,01fH,000H
+	defb 063H,063H,063H,07fH,063H,063H,063H,000H
+	defb 07eH,018H,018H,018H,018H,018H,07eH,000H
+	defb 006H,006H,006H,006H,006H,066H,03cH,000H
+	defb 063H,066H,06cH,078H,06cH,066H,063H,000H
+	defb 060H,060H,060H,060H,060H,060H,07eH,000H
+	defb 07eH,06bH,06bH,06bH,06bH,06bH,06bH,000H
+	defb 063H,073H,07bH,07fH,06fH,067H,063H,000H
+	defb 03eH,063H,063H,063H,063H,063H,03eH,000H
+	defb 07eH,063H,063H,063H,07eH,060H,060H,000H
+	defb 03eH,063H,063H,063H,06bH,066H,03bH,000H
+	defb 07eH,063H,063H,067H,07cH,06eH,067H,000H
+	defb 03eH,063H,060H,03eH,003H,063H,03eH,000H
+	defb 07eH,018H,018H,018H,018H,018H,018H,000H
+	defb 063H,063H,063H,063H,063H,063H,03eH,000H
+	defb 063H,063H,063H,036H,036H,01cH,01cH,000H
+	defb 063H,06bH,06bH,06bH,06bH,06bH,07eH,000H
+	defb 063H,063H,036H,01cH,036H,063H,063H,000H
+	defb 066H,066H,066H,03cH,018H,018H,018H,000H
+	defb 07fH,006H,00cH,018H,030H,060H,07fH,000H
+	defb 03cH,020H,020H,020H,020H,020H,03cH,000H
+	defb 022H,022H,014H,03eH,008H,03eH,008H,000H
+	defb 03cH,004H,004H,004H,004H,004H,03cH,000H
+	defb 008H,014H,022H,000H,000H,000H,000H,000H
+	defb 000H,000H,000H,000H,000H,000H,07eH,000H
+	; Logo
+	defb	00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h
+	defb	0f0h, 0f0h, 0f0h, 0f0h, 00h, 00h, 00h, 00h
+	defb	00h, 00h, 00h, 00h, 0f0h, 0f0h, 0f0h, 0f0h
+	defb	0f0h, 0f0h, 0f0h, 0f0h, 0f0h, 0f0h, 0f0h, 0f0h
+	defb	0fh, 0fh, 0fh, 0fh, 00h, 00h, 00h, 00h
+	defb	0ffh, 0ffh, 0ffh, 0ffh, 00h, 00h, 00h, 00h
+	defb	0fh, 0fh, 0fh, 0fh, 0f0h, 0f0h, 0f0h, 0f0h
+	defb	0ffh, 0ffh, 0ffh, 0ffh, 0f0h, 0f0h, 0f0h, 0f0h
+	defb	00h, 00h, 00h, 00h, 0fh, 0fh, 0fh, 0fh
+	defb	0f0h, 0f0h, 0f0h, 0f0h, 0fh, 0fh, 0fh, 0fh
+	defb	00h, 00h, 00h, 00h, 0ffh, 0ffh, 0ffh, 0ffh
+	defb	0f0h, 0f0h, 0f0h, 0f0h, 0ffh, 0ffh, 0ffh, 0ffh
+	defb	0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh
+	defb	0ffh, 0ffh, 0ffh, 0ffh, 0fh, 0fh, 0fh, 0fh
+	defb	0fh, 0fh, 0fh, 0fh, 0ffh, 0ffh, 0ffh, 0ffh
+	defb	0ffh, 0ffh, 0ffh, 0ffh, 0ffh, 0ffh, 0ffh, 0ffh
+    ; 8dot
+	defb	00h, 0aah, 44h, 0aah, 00h, 0aah, 11h, 0aah
+	defb	00h, 0eeh, 0eeh, 0eeh, 00h, 0bbh, 0bbh, 0bbh
+	defb	00h, 0feh, 0feh, 0feh, 0feh, 0feh, 0feh, 0feh
+	defb	00h, 0eeh, 0eeh, 0eeh, 0eeh, 0eeh, 0eeh, 00h
+	defb	00h, 0e0h, 0e0h, 0e0h, 0e0h, 0e0h, 0e0h, 00h
+    ; fort
+	defb	00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h
+	defb	00h, 00h, 00h, 00h, 00h, 00h, 00h, 3eh
+	defb	02h, 06h, 1fh, 3ah, 3fh, 7ah, 7fh, 00h
+	defb	00h, 00h, 80h, 00h, 80h, 00h, 80h, 00h
+	defb	00h, 00h, 01h, 05h, 05h, 05h, 01h, 1eh
+	defb	00h, 0ech, 61h, 6dh, 0c1h, 1ch, 3eh, 3eh
+	defb	0cdh, 0ch, 0b1h, 0b6h, 0b6h, 01h, 0b1h, 0b6h
+	defb	80h, 20h, 0b0h, 18h, 0d8h, 0e8h, 0e0h, 0c8h
+	defb	25h, 3dh, 3ch, 00h, 7fh, 66h, 0ffh, 0e7h
+	defb	0dch, 0c0h, 1eh, 00h, 0ffh, 01h, 0ffh, 9eh
+	defb	8eh, 70h, 0f9h, 00h, 0ffh, 0c0h, 0ffh, 79h
+	defb	18h, 00h, 0fch, 00h, 0feh, 1eh, 0ffh, 0e7h
+	defb	00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h
+	defb	0ch, 1ch, 7fh, 1fh, 0ffh, 00h, 3eh, 7eh
+	defb	00h, 00h, 00h, 80h, 80h, 00h, 00h, 00h
+	defb	00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h
+	defb	00h, 1fh, 10h, 3fh, 33h, 33h, 7fh, 40h
+	defb	00h, 0ffh, 0fbh, 0abh, 0abh, 0aah, 0fbh, 40h
+	defb	03h, 0f7h, 04h, 0f7h, 13h, 0fch, 0fh, 0f6h
+	defb	80h, 0d8h, 40h, 0dch, 9ch, 5ch, 0deh, 72h
+	defb	4eh, 00h, 0ffh, 80h, 3fh, 41h, 58h, 3fh
+	defb	7fh, 00h, 0ffh, 01h, 0fch, 82h, 1ah, 0fch
+	defb	0ffh, 00h, 0ffh, 80h, 3fh, 41h, 58h, 3fh
+	defb	0feh, 00h, 0ffh, 01h, 0fch, 82h, 1ah, 0fch
+
+
+SpritePattern: public SpritePattern
+	defb	0fh, 1fh, 1eh, 1fh, 0fh, 01h, 38h, 18h
+	defb	01h, 1bh, 0bh, 00h, 0eh, 18h, 30h, 38h
+	defb	0c0h, 0e0h, 00h, 00h, 0e0h, 0c0h, 10h, 00h
+	defb	0e0h, 80h, 00h, 00h, 60h, 70h, 30h, 38h
+	defb	0fh, 1fh, 1eh, 1fh, 0fh, 01h, 1ch, 0ch
+	defb	00h, 0dh, 05h, 00h, 0dh, 05h, 03h, 03h
+	defb	0c0h, 0e0h, 00h, 00h, 0e0h, 0c0h, 00h, 00h
+	defb	0f0h, 0c0h, 80h, 00h, 80h, 80h, 00h, 80h
+	defb	03h, 07h, 00h, 00h, 07h, 03h, 08h, 03h
+	defb	3bh, 0bh, 00h, 01h, 06h, 0eh, 0ch, 1ch
+	defb	0f0h, 0f8h, 38h, 78h, 0f0h, 80h, 1ch, 0d8h
+	defb	0c0h, 0cch, 0ch, 8ch, 70h, 18h, 0ch, 1ch
+	defb	03h, 07h, 00h, 00h, 07h, 03h, 00h, 03h
+	defb	1bh, 02h, 00h, 00h, 01h, 01h, 00h, 01h
+	defb	0f0h, 0f8h, 38h, 78h, 0f0h, 80h, 38h, 0b0h
+	defb	00h, 0e0h, 0c0h, 00h, 0b0h, 0a0h, 0c0h, 0c0h
+	defb	03h, 07h, 0fh, 0fh, 0e8h, 6ch, 07h, 30h
+	defb	37h, 37h, 11h, 14h, 0ch, 12h, 1eh, 0ch
+	defb	0c0h, 0e0h, 0f0h, 0f0h, 10h, 37h, 0e6h, 00h
+	defb	0e6h, 0e6h, 80h, 48h, 78h, 30h, 00h, 00h
+	defb	03h, 07h, 0fh, 0fh, 08h, 0ech, 67h, 00h
+	defb	37h, 37h, 31h, 12h, 16h, 0ch, 00h, 00h
+	defb	0c0h, 0e0h, 0f0h, 0f0h, 17h, 36h, 0e0h, 06h
+	defb	0e6h, 0e0h, 80h, 30h, 30h, 48h, 78h, 30h
+	defb	03h, 07h, 0fh, 0fh, 0fh, 0efh, 67h, 00h
+	defb	6fh, 67h, 07h, 00h, 12h, 1eh, 00h, 00h
+	defb	0c0h, 0e0h, 0f0h, 0f0h, 0f7h, 0f6h, 0e0h, 06h
+	defb	0f6h, 0e0h, 0e0h, 00h, 30h, 30h, 48h, 78h
+	defb	03h, 07h, 0fh, 0fh, 0efh, 6fh, 07h, 60h
+	defb	6fh, 07h, 07h, 00h, 0ch, 0ch, 12h, 1eh
+	defb	0c0h, 0e0h, 0f0h, 0f0h, 0f0h, 0f7h, 0e6h, 00h
+	defb	0f6h, 0e6h, 0e0h, 00h, 48h, 78h, 00h, 00h
+	defb	07h, 0fh, 1fh, 1fh, 1fh, 00h, 1bh, 18h
+	defb	01h, 1bh, 0bh, 00h, 0eh, 18h, 30h, 38h
+	defb	80h, 0c0h, 20h, 20h, 0e0h, 00h, 0c0h, 00h
+	defb	0e0h, 80h, 00h, 00h, 60h, 70h, 30h, 38h
+	defb	07h, 0fh, 1fh, 1fh, 1fh, 00h, 0dh, 0ch
+	defb	00h, 0dh, 05h, 00h, 0dh, 05h, 03h, 03h
+	defb	80h, 0c0h, 20h, 20h, 0e0h, 00h, 0c0h, 00h
+	defb	0f0h, 0c0h, 80h, 00h, 80h, 80h, 00h, 80h
+	defb	01h, 03h, 04h, 04h, 07h, 00h, 03h, 03h
+	defb	3bh, 0bh, 00h, 01h, 06h, 0eh, 0ch, 1ch
+	defb	0e0h, 0f0h, 0f8h, 0f8h, 0f8h, 00h, 0d8h, 0d8h
+	defb	0c0h, 0cch, 0ch, 8ch, 70h, 18h, 0ch, 1ch
+	defb	01h, 03h, 04h, 04h, 07h, 00h, 03h, 03h
+	defb	1bh, 02h, 00h, 00h, 01h, 01h, 00h, 01h
+	defb	0e0h, 0f0h, 0f8h, 0f8h, 0f8h, 00h, 0b8h, 0b0h
+	defb	00h, 0e0h, 0c0h, 00h, 0b0h, 0a0h, 0c0h, 0c0h
+	defb	03h, 07h, 0fh, 0eh, 6eh, 6fh, 00h, 37h
+	defb	37h, 37h, 11h, 14h, 0ch, 12h, 1eh, 0ch
+	defb	0c0h, 0e0h, 0f0h, 70h, 70h, 0f6h, 06h, 0f0h
+	defb	0f6h, 0e6h, 80h, 48h, 78h, 30h, 00h, 00h
+	defb	03h, 07h, 0fh, 0eh, 0eh, 6fh, 60h, 0fh
+	defb	37h, 37h, 31h, 12h, 16h, 0ch, 00h, 00h
+	defb	0c0h, 0e0h, 0f0h, 70h, 76h, 0f6h, 00h, 0f6h
+	defb	0f6h, 0e0h, 80h, 30h, 30h, 48h, 78h, 30h
+	defb	03h, 07h, 0fh, 0fh, 0fh, 6fh, 60h, 0fh
+	defb	6fh, 60h, 07h, 00h, 12h, 1eh, 00h, 00h
+	defb	0c0h, 0e0h, 0f0h, 0f0h, 0f6h, 0f6h, 00h, 0f6h
+	defb	0f6h, 00h, 0e0h, 00h, 30h, 30h, 48h, 78h
+	defb	03h, 07h, 0fh, 0fh, 6fh, 6fh, 00h, 6fh
+	defb	6fh, 00h, 07h, 00h, 0ch, 0ch, 12h, 1eh
+	defb	0c0h, 0e0h, 0f0h, 0f0h, 0f0h, 0f6h, 06h, 0f0h
+	defb	0f6h, 06h, 0e0h, 00h, 48h, 78h, 00h, 00h
+	defb	00h, 00h, 00h, 00h, 00h, 00h, 00h, 7eh
+	defb	00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h
+	defb	00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h
+	defb	00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h
+	defb	00h, 01h, 01h, 01h, 01h, 01h, 01h, 00h
+	defb	00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h
+	defb	00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h
+	defb	00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h
+	defb	01h, 21h, 33h, 3eh, 24h, 0e1h, 77h, 3bh
+	defb	1bh, 30h, 0e2h, 6eh, 3eh, 1bh, 11h, 01h
+	defb	04h, 9ch, 0f8h, 0f9h, 6fh, 0eh, 9ch, 0b8h
+	defb	0dch, 0cfh, 0eh, 48h, 0ech, 0fch, 8ch, 00h
+	defb	00h, 00h, 08h, 0eh, 07h, 07h, 06h, 03h
+	defb	03h, 1fh, 0f3h, 78h, 3eh, 1fh, 0fh, 06h
+	defb	02h, 07h, 07h, 0fh, 9dh, 0fch, 78h, 38h
+	defb	12h, 82h, 9fh, 3bh, 79h, 19h, 98h, 30h
+	defb	00h, 00h, 01h, 87h, 8fh, 0dfh, 0fdh, 79h
+	defb	71h, 25h, 0ch, 9ch, 0f8h, 19h, 11h, 38h
+	defb	60h, 0e0h, 0c0h, 0c0h, 0c0h, 80h, 8eh, 0bch
+	defb	0f8h, 0f8h, 30h, 0e0h, 0c0h, 0c0h, 0e0h, 0f0h
+	defb	04h, 09h, 39h, 73h, 0e0h, 7fh, 3fh, 0eh
+	defb	06h, 02h, 03h, 03h, 02h, 00h, 00h, 00h
+	defb	64h, 0ceh, 9eh, 0ffh, 7dh, 21h, 6dh, 1ch
+	defb	7eh, 0eeh, 0ceh, 07h, 07h, 03h, 02h, 02h
+	defb	7fh, 60h, 0f0h, 0f3h, 9bh, 09h, 21h, 38h
+	defb	7ch, 0ffh, 0e3h, 0c1h, 80h, 00h, 00h, 00h
+	defb	38h, 0ch, 0feh, 0ffh, 0f0h, 80h, 80h, 0c0h
+	defb	0c0h, 0c0h, 0e0h, 0e0h, 60h, 00h, 00h, 00h
---
+++ myvision/battlot/Chars.h
@@ -0,0 +1,17 @@
+
+constexpr byte Char_Space = 0x0;
+constexpr byte Char_Ascii = 0x0;
+constexpr byte Char_Logo = 0x40;
+constexpr byte Char_Wall = 0x50;
+constexpr byte Char_HardWall = 0x52;
+constexpr byte Char_Meter = 0x53;
+constexpr byte Char_MyFort = 0x55;
+constexpr byte Char_EnemyFort = 0x61;
+constexpr byte Char_End = 0x6D;
+
+constexpr byte Pattern_MyRobo = 0;
+constexpr byte Pattern_EnemyRobo = 8;
+constexpr byte Pattern_Bullet = 16;
+constexpr byte Pattern_SmallBang = 18;
+constexpr byte Pattern_LargeBang = 19;
+constexpr byte Pattern_End = 23;
