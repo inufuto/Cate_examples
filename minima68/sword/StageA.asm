69d507f097565016ba6e40a78435de2746496bf5
Inufuto <mxb03523@nifty.com>
2026-04-27 22:23:57 +0900

    Add Minima68 SwordWork

---
+++ minima68/sword/StageA.asm
@@ -0,0 +1,82 @@
+include "Direct.inc"
+include "Chars.inc"
+
+ext StageMap_
+
+ColumnCount equ 16
+RowCount equ 11
+
+pMap equ @Temp@Word
+shiftCount equ @Temp@Byte2
+mask equ @Temp@Byte3
+cellValue equ @Temp@Byte4
+
+dseg
+SetCellBits_@Param1: public SetCellBits_@Param1
+GetCell_@Param1: public GetCell_@Param1
+SetCellBits_row:
+    defb 1
+SetCellBits_@Param2: public SetCellBits_@Param2
+SetCellBits_cell:
+    defb 1
+
+cseg
+MapPtr:
+    psha    
+        lsra | lsra
+        ldab SetCellBits_row
+        aslb ;*2
+        aslb ;*4
+        aba | clrb
+        adda #low StageMap_ | staa pMap+1
+        adcb #high StageMap_ | stab pMap
+        ldx pMap
+    pula
+    anda #3
+rts
+
+; void SetCellBits(byte column, byte row, byte cell);
+cseg
+SetCellBits_: public SetCellBits_
+    psha | pshb
+        bsr MapPtr
+        staa shiftCount
+        ldaa SetCellBits_cell
+        ldab #$03
+        tst shiftCount
+        do | while ne
+            asla | asla
+            aslb | aslb
+            dec shiftCount
+        wend
+        staa cellValue
+        comb
+        stab mask
+        ldaa 0,x
+        anda mask
+        oraa cellValue
+        staa 0,x
+    pulb | pula
+rts
+        
+
+; byte GetCell(byte column, byte row);
+cseg
+GetCell_: public GetCell_
+    pshb
+                ldab SetCellBits_row
+                if ne
+                    _deb: public _deb
+                    nop
+                endif
+        bsr MapPtr
+        ldab 0,x
+        tsta
+        do | while ne
+            lsrb | lsrb
+            deca
+        wend
+        tba
+        anda #3
+    pulb
+rts
---
+++ minima68/sword/Status.c
@@ -0,0 +1,137 @@
+#include "Status.h"
+#include "Vram.h"
+#include "Main.h"
+#include "Print.h"
+#include "Chars.h"
+#include "Sprite.h"
+
+static ptr<byte> PrintS(ptr<byte> pVram, ptr<byte> p)
+{
+    byte c;
+    while ((c = *p) != 0) {
+        pVram = PrintC(pVram, c);
+        ++p;
+    }
+    return pVram;
+}
+
+void PrintStatus()
+{
+    PrintS(Vram + 1 * VramStep, "SCORE");
+    PrintS(Vram + 7 * VramStep, "HI-SCORE");
+    PrintS(Vram + 16 * VramStep, "STAGE");
+    PrintByteNumber2(Vram + VramRowSize + 18 * VramStep, CurrentStage + 1);
+
+    PrintScore();
+    PrintRemain();
+}
+
+void PrintScore()
+{
+    ptr<byte> pVram;
+    
+    pVram = PrintNumber5(Vram + VramRowSize, Score);
+    PrintC(pVram, '0');
+
+    pVram = PrintNumber5(Vram + VramRowSize + 9 * VramStep, HiScore);
+    PrintC(pVram, '0');
+}
+
+void PrintRemain()
+{
+    ptr<byte> pVram = Vram + 28 * VramStep;
+    if (RemainCount > 1) {
+        byte i;
+        i = RemainCount - 1;
+        if (i > 2) {
+            pVram = Put2C(pVram, Char_Remain);
+            Erase2(pVram);
+            Put(pVram + VramRowSize, i + 0x10);
+        }
+        else {
+            do {
+                pVram = Put2C(pVram, Char_Remain);
+                --i;
+            } while (i != 0);
+        }
+    }
+    if (RemainCount < 3) {
+        Erase2(pVram);
+    }
+}
+
+void PrintGameOver()
+{
+    PrintS(Vram + VramRowSize * 12 + 11 * VramStep, "GAME OVER");
+}
+
+void Title()
+{
+    static const byte[] TitleBytes = {
+		0x00, 0x4d, 0x44, 0x4a, 
+		0x00, 0x4c, 0x49, 0x41, 
+		0x00, 0x49, 0x00, 0x4e, 
+		0x00, 0x43, 0x44, 0x40, 
+		0x4b, 0x42, 0x41, 0x4e, 
+		0x4b, 0x42, 0x42, 0x4e, 
+		0x43, 0x44, 0x44, 0x40, 
+		0x00, 0x00, 0x00, 0x00, 
+		0x47, 0x46, 0x4a, 0x4b, 
+		0x4b, 0x42, 0x4e, 0x4b, 
+		0x00, 0x44, 0x40, 0x43, 
+		0x00, 0x00, 0x00, 0x00, 
+		0x46, 0x4a, 0x4b, 0x46, 
+		0x4a, 0x46, 0x4b, 0x42, 
+		0x40, 0x44, 0x43, 0x44, 
+		0x00, 0x00, 0x00, 0x00, 
+		0x4a, 0x00, 0x4e, 0x47, 
+		0x4e, 0x00, 0x4e, 0x4b, 
+		0x40, 0x00, 0x4e, 0x4b, 
+		0x00, 0x00, 0x44, 0x44, 
+		0x4b, 0x42, 0x00, 0x00, 
+		0x4b, 0x42, 0x4d, 0x4c, 
+		0x4b, 0x42, 0x4e, 0x4b, 
+		0x44, 0x00, 0x43, 0x44, 
+		0x00, 0x00, 0x00, 0x00, 
+		0x41, 0x4e, 0x4c, 0x41, 
+		0x42, 0x4e, 0x4d, 0x40, 
+		0x00, 0x44, 0x43, 0x40, 
+		0x00, 0x00, 0x00, 0x00, 
+		0x4e, 0x47, 0x46, 0x00, 
+		0x4e, 0x4c, 0x41, 0x00, 
+		0x44, 0x00, 0x44, 0x00, 
+    };
+    ClearScreen(); 
+    HideAllSprites();
+    PrintStatus();
+    {
+        ptr<byte> p;
+        ptr<byte> pVram;
+        pVram = Vram + VramRowSize * 7 + (32 - 8 * 4) / 2 * VramStep;
+        p = TitleBytes;
+        repeat (8) {
+            repeat (4) {
+                repeat (4) {
+                    pVram = Put(pVram, *p);
+                    ++p;
+                }
+                pVram += VramRowSize - 4 * VramStep;
+            }
+            pVram += 4 * VramStep - VramRowSize * 4;
+        }
+    }
+    PrintS(Vram + VramRowSize * 19 + 12 * VramStep, "Z:START");
+    PrintS(Vram + VramRowSize * 20 + 12 * VramStep, "X:CONTINUE");
+    PrintS(Vram + VramRowSize * 23 + 20 * VramStep, "INUFUTO 2026");
+    // {
+    //     ptr<byte> pVram = Vram + VramRowSize * 2;
+    //     byte c = 0;
+    //     repeat (Char_End) {
+    //         pVram = Put(pVram, c);
+    //         ++c;
+    //         if ((c & 15) == 0) {
+    //             pVram += VramRowSize - 16 * VramStep;
+    //         }
+    //     }
+    // }
+}
