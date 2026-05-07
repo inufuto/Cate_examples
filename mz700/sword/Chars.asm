de1e70730e38c69c9e39e053f470ffa5c2dcf0f1
Inufuto <mxb03523@nifty.com>
2026-05-07 22:02:10 +0900

    Add MZ-700 SwordWork

---
+++ mz700/sword/Chars.asm
@@ -0,0 +1,38 @@
+cseg
+
+Chars: public Chars
+;	logo	Patterns
+	defb	00h, 00h, 0f1h, 60h, 0f4h, 60h, 0f5h, 60h
+	defb	0f2h, 60h, 0f3h, 60h, 0f6h, 60h, 0f7h, 60h
+	defb	0f8h, 60h, 0f9h, 60h, 0fch, 60h, 0fdh, 60h
+	defb	0fah, 60h, 0fbh, 60h, 0feh, 60h, 0ffh, 60h
+;	fence	Patterns
+	defb	3ch, 10h, 70h, 10h
+;	2	Patterns
+	defb	5ah, 10h, 5ah, 10h, 5ah, 10h, 5ah, 10h
+	defb	3ch, 62h, 3ch, 62h, 6fh, 62h, 6eh, 62h
+	defb	0c2h, 61h, 0c2h, 61h, 0c2h, 61h, 0c2h, 61h
+;	sprite	Patterns
+	defb	3bh, 50h, 7bh, 50h, 0f8h, 50h, 0f9h, 50h
+	defb	3bh, 50h, 7bh, 50h, 00h, 70h, 0f5h, 50h
+	defb	43h, 50h, 00h, 50h, 0f6h, 50h, 0f4h, 50h
+	defb	00h, 70h, 00h, 70h, 78h, 70h, 1bh, 70h
+	defb	3bh, 50h, 7bh, 50h, 0f6h, 50h, 0f4h, 50h
+	defb	3bh, 50h, 7bh, 50h, 0fah, 50h, 00h, 50h
+	defb	00h, 50h, 43h, 50h, 0f8h, 50h, 0f9h, 50h
+	defb	00h, 70h, 00h, 70h, 1bh, 70h, 78h, 70h
+	defb	3ah, 50h, 0f8h, 50h, 7ah, 50h, 0f9h, 50h
+	defb	3ah, 50h, 00h, 50h, 7ah, 50h, 0f3h, 50h
+	defb	43h, 50h, 0f6h, 50h, 00h, 50h, 0f2h, 50h
+	defb	00h, 70h, 79h, 70h, 00h, 70h, 1bh, 70h
+	defb	3ah, 50h, 0f6h, 50h, 7ah, 50h, 0f2h, 50h
+	defb	3ah, 50h, 0fch, 50h, 7ah, 50h, 00h, 50h
+	defb	00h, 50h, 0f8h, 50h, 43h, 50h, 0f9h, 50h
+	defb	00h, 70h, 1bh, 70h, 00h, 70h, 79h, 70h
+	defb	0fbh, 70h, 0fbh, 70h, 0f7h, 70h, 0f7h, 70h
+	defb	0f1h, 07h, 0f4h, 07h, 0f2h, 07h, 00h, 07h
+	defb	21h, 70h, 20h, 70h, 00h, 00h, 20h, 70h
+	defb	22h, 70h, 20h, 70h, 00h, 00h, 20h, 70h
+	defb	24h, 70h, 20h, 70h, 00h, 00h, 20h, 70h
+	defb	28h, 70h, 20h, 70h, 00h, 00h, 20h, 70h
+	defb	21h, 14h, 00h, 14h, 15h, 14h, 10h, 14h
---
+++ mz700/sword/Startup.asm
@@ -0,0 +1,77 @@
+interrupt_interval equ 120 / 2
+
+ext InitSound, SoundHandler
+ext Main_
+
+dseg
+TimerCount: public TimerCount
+defb 0 
+
+cseg
+call InitSound
+di
+    ld hl,(1038h+1) ; interrupt handler
+    ld (Handler_end+1),hl
+    ld hl,Handler
+    ld (1038h+1),hl
+
+        
+    ld hl,0e007h
+    ld (hl),0b0h
+    ld (hl),074h
+    dec l
+    ld (hl),1+1
+    ld (hl),0
+    dec l
+    ld (hl),interrupt_interval
+    ld (hl),0
+    ld a,5
+    ld (0e003h),a
+
+    xor a
+    ld (TimerCount),a
+ei
+jp Main_
+
+
+Handler: public Handler
+    push af | push hl
+        ld hl,0e002h
+        do
+            ld a,(hl)
+            and 80h
+        while nz | wend
+        ; do
+        ;     ld a,(hl)
+        ;     and 80h
+        ; while z | wend
+
+        ld hl,TimerCount
+        inc (hl)
+
+        call SoundHandler
+        ld a,1+1
+        ld (0e006h),a
+        xor a
+        ld (0e006h),a
+    pop hl | pop af
+    ei
+    ret
+Handler_end:
+    jp 0
+
+
+WaitTimer_:
+    public WaitTimer_
+    push af | push de
+        ld e,a
+        do
+            ld a,(TimerCount)
+            cp e
+        while c | wend
+        di
+            xor a
+            ld (TimerCount),a
+        ei
+    pop de | pop af
+ret
