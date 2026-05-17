0adb50daa3b1b785419726434f3bbaaa1b2729f3
Inufuto <mxb03523@nifty.com>
2026-05-17 10:08:53 +0900

    Add GameGear SwordWork

---
+++ gg/sword/StageA.asm
@@ -0,0 +1,57 @@
+include "Vram.inc"
+include "Chars.inc"
+
+ext StageMap_
+
+ColumnCount equ 10
+RowCount equ 7
+
+cseg
+MapPtr:
+    ld b,a
+    rrca | rrca | and 0fh 
+    ld d,a
+    ld a,e
+    add a,a ;*2
+    add a,e ;*3
+    add a,d
+    add a,low StageMap_ | ld l,a
+    ld a,high StageMap_ | adc a,0 | ld h,a
+    ld a,b
+    and 3
+ret
+
+; void SetCellBits(byte column, byte row, byte cell);
+cseg
+SetCellBits_: public SetCellBits_
+    push af | push hl | push de | push bc
+        call MapPtr
+        ld e,0fch
+        or a
+        do | while nz
+            sla c | sla c
+            rlc e | rlc e
+            dec a
+        wend
+        ld a,(hl)
+        and e
+        or c
+        ld (hl),a
+    pop bc | pop de | pop hl | pop af
+ret
+
+; byte GetCell(byte column, byte row);
+cseg
+GetCell_: public GetCell_
+    push hl | push de | push bc
+        call MapPtr
+        ld e,(hl)
+        or a
+        do | while nz
+            srl e | srl e
+            dec a
+        wend
+        ld a,e
+        and 03h
+    pop bc | pop de | pop hl
+ret
