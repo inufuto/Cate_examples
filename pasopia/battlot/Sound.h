f82f6be503ed542cd775edc8d9441f16bdfcdb5d
Inufuto <mxb03523@nifty.com>
2026-05-25 02:09:14 +0900

    Add PASOPIA BATTLOT

---
+++ pasopia/battlot/Sound.h
@@ -0,0 +1,9 @@
+extern void Sound_Fire();
+extern void Sound_Beep();
+extern void Sound_Hit();
+extern void Sound_Loose();
+extern void Sound_Start();
+extern void Sound_Clear();
+extern void Sound_GameOver();
+extern void StartBGM();
+extern void StopBGM();
