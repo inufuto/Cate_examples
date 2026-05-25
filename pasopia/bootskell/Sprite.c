6ead17077159c699b67e3ba72391aeb07d6133d7
Inufuto <mxb03523@nifty.com>
2026-05-25 11:22:17 +0900

    Add PASOPIA Bootskell

---
+++ pasopia/bootskell/Sprite.c
@@ -0,0 +1,81 @@
+#include "Sprite.h"
+#include "Chars.h"
+#include "VVram.h"
+#include "Vram.h"
+
+constexpr byte InvalidCode = 0xff;
+Sprite[SpriteCount] Sprites;
+
+void HideAllSprites()
+{
+    ptr<Sprite> p;
+    for (p : Sprites) {
+        p->code = InvalidCode;
+        p->oldCode = InvalidCode;
+    }
+}
+
+
+void ShowSprite(ptr<Movable> pMovable, byte code)
+{
+    ptr<Sprite> p;
+    p = Sprites + pMovable->sprite;
+    p->x = pMovable->x;
+    p->y = pMovable->y;
+    p->code = code;
+}
+
+
+void HideSprite(byte index) 
+{
+    ptr<Sprite> p;
+    p = Sprites + index;
+    p->code = InvalidCode;
+}
+
+
+void EraseSprites() 
+{
+    ptr<Sprite> p;
+    for (p : Sprites) {
+        if (p->oldCode != InvalidCode && (
+            p->oldX != p->x || 
+            p->oldY != p->y ||
+            p->code == InvalidCode
+        )) {
+            word offset;
+            ptr<byte> pFront, pBack;
+            byte y;
+            y = p->oldY;
+            offset = VVramOffset(p->oldX, y);
+            pFront = VVramFront + offset;
+            pBack = VVramBack + offset;
+            pFront[0] = pBack[0];
+            pFront[1] = pBack[1];
+            pFront[VVramWidth] = pBack[VVramWidth];
+            pFront[VVramWidth + 1] = pBack[VVramWidth + 1];
+        }
+    }
+}
+
+
+void DrawSprites()
+{
+    ptr<Sprite> p;
+    for (p : Sprites) {
+        if (p->code != InvalidCode) {
+            ptr<byte> pFront;
+            byte y, c;
+            y = p->y;
+            pFront = VVramFront + VVramOffset(p->x, y);
+            c = p->code;
+            pFront[0] = c; 
+            ++c; pFront[1] = c;
+            ++c; pFront[VVramWidth] = c;
+            ++c; pFront[VVramWidth + 1] = c;
+        }
+        p->oldX = p->x;
+        p->oldY = p->y;
+        p->oldCode = p->code;
+    }
+}
