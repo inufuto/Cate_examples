2cc1ab235c2f2b0aba6971a27a071ccf49b1a62d
Inufuto <mxb03523@nifty.com>
2026-05-25 19:48:51 +0900

    Add PASOPIA CRACKY

---
+++ pasopia/cracky/Vram.h
@@ -0,0 +1,15 @@
+constexpr word Vram = 1;
+
+constexpr byte VramWidth = 81;
+constexpr byte VramHeight = 25;
+constexpr byte VramStep = VramWidth / 40;
+constexpr byte VramRowSize = VramWidth;
+
+extern void ClearScreen();
+extern word Put(word vram, byte c);
+extern void VVramToVramAll();
+extern void VVramToVramChanged();
+
+extern void InitVram();
+extern word PrintC(word vram, byte c);
+extern void Put2C(word vram, byte c);
