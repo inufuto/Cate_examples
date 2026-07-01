cd7abf78cb554632be7dd9ff74f4c7799cfd6ecc
Inufuto <mxb03523@nifty.com>
2026-07-01 13:46:12 +0900

    Add PC-98XA SwordWork

---
+++ pc98xa/sword/Status.c
@@ -0,0 +1,129 @@
+#include "Status.h"
+#include "Vram.h"
+#include "Main.h"
+#include "Print.h"
+#include "Chars.h"
+#include "Sprite.h"
+
+static word PrintS(word address, ptr<byte> p)
+{
+    byte c;
+    while ((c = *p) != 0) {
+        address = PrintC(address, c);
+        ++p;
+    }
+    return address;
+}
+
+void PrintStatus()
+{
+    PrintS(Vram + 0, "SCORE");
+    PrintS(Vram + 14, "HI-SCORE");
+    PrintS(Vram + 32, "STAGE");
+    PrintByteNumber2(Vram + VramWidth + 38, CurrentStage + 1);
+    PrintScore();
+    PrintRemain();
+}
+
+void PrintScore()
+{
+    word vram;
+    
+    vram = PrintNumber5(Vram + 6, Score);
+    PrintC(vram, '0');
+
+    vram = PrintNumber5(Vram + 23, HiScore);
+    PrintC(vram, '0');
+}
+
+void PrintRemain()
+{
+    word vram;
+    vram = Vram + 60;
+    if (RemainCount > 1) {
+        byte i;
+        i = RemainCount - 1;
+        if (i > 2) {
+            vram = Put(vram, Char_Remain);
+            Put(vram, Char_Space);
+            vram = PrintC(vram, i + '0');
+        }
+        else {
+            do {
+                vram = Put(vram, Char_Remain);
+                --i;
+            } while (i > 0);
+        }
+    }
+    vram = Put(vram, Char_Space);
+}
+
+
+void PrintGameOver()
+{
+    PrintS(Vram + VramRowSize * 12 + 27, "GAME OVER");
+}
+
+void PrintTimeUp() 
+{
+    PrintS(Vram + VramRowSize * 12 + 28, "TIME UP");
+}
+
+void Title()
+{
+    ClearScreen(); 
+    HideAllSprites();
+    PrintStatus();
+    {
+        static const byte[] TitleBytes = {
+			0x00, 0x0e, 0x05, 0x0b, 
+			0x00, 0x0d, 0x0a, 0x02, 
+			0x00, 0x0a, 0x00, 0x0f, 
+			0x00, 0x04, 0x05, 0x01, 
+			0x0c, 0x03, 0x02, 0x0f, 
+			0x0c, 0x03, 0x03, 0x0f, 
+			0x04, 0x05, 0x05, 0x01, 
+			0x00, 0x00, 0x00, 0x00, 
+			0x08, 0x07, 0x0b, 0x0c, 
+			0x0c, 0x03, 0x0f, 0x0c, 
+			0x00, 0x05, 0x01, 0x04, 
+			0x00, 0x00, 0x00, 0x00, 
+			0x07, 0x0b, 0x0c, 0x07, 
+			0x0b, 0x07, 0x0c, 0x03, 
+			0x01, 0x05, 0x04, 0x05, 
+			0x00, 0x00, 0x00, 0x00, 
+			0x0b, 0x00, 0x0f, 0x08, 
+			0x0f, 0x00, 0x0f, 0x0c, 
+			0x01, 0x00, 0x0f, 0x0c, 
+			0x00, 0x00, 0x05, 0x05, 
+			0x0c, 0x03, 0x00, 0x00, 
+			0x0c, 0x03, 0x0e, 0x0d, 
+			0x0c, 0x03, 0x0f, 0x0c, 
+			0x05, 0x00, 0x04, 0x05, 
+			0x00, 0x00, 0x00, 0x00, 
+			0x02, 0x0f, 0x0d, 0x02, 
+			0x03, 0x0f, 0x0e, 0x01, 
+			0x00, 0x05, 0x04, 0x01, 
+			0x00, 0x00, 0x00, 0x00, 
+			0x0f, 0x08, 0x07, 0x00, 
+			0x0f, 0x0d, 0x02, 0x00, 
+			0x05, 0x00, 0x05, 0x00, 
+        };
+        constexpr byte LogoLength = 8;
+        word vram = Vram + VramRowSize * 7 + (32 - LogoLength * 4) / 2 * VramStep;
+        ptr<byte> p = TitleBytes;
+        repeat (LogoLength) {
+            repeat (4) {
+                repeat (4) {
+                    vram = Put(vram, *p);
+                    ++p;
+                }
+                vram += VramRowSize - 4 * VramStep;
+            }
+            vram += 4 * VramStep - VramRowSize * 4;
+        }
+    }
+    PrintS(Vram + VramRowSize * 19 + 25, "PUSH SPACE KEY");
+    PrintS(Vram + VramRowSize * 20 + 25, "OR GRPH KEY");
+    PrintS(Vram + VramRowSize * 22 + 52, "INUFUTO 2026");
+}
