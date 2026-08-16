e1e8cbab72318e992980967f5dd42e8635195d8a
Inufuto <mxb03523@nifty.com>
2026-08-16 22:15:10 +0900

    Add MZ-80B UCHOTOS

---
+++ mz80b/uchotos/Chars.asm
@@ -0,0 +1,69 @@
+cseg
+
+CharPattern: public CharPattern
+; ascii 31
+	; " -0123456789ABCEFGHIKMNOPRSTUVY"
+	defb 00h,00h,00h,00h,00h,00h,00h,00h
+	defb 00h,00h,00h,7eh,00h,00h,00h,00h
+	defb 7ch,0c6h,0e6h,0d6h,0ceh,0c6h,7ch,00h
+	defb 18h,1eh,18h,18h,18h,18h,7eh,00h
+	defb 7ch,0c6h,0e0h,78h,3ch,0eh,0feh,00h
+	defb 7ch,0c6h,0c0h,78h,0c0h,0c6h,7ch,00h
+	defb 60h,70h,78h,6ch,66h,0feh,60h,00h
+	defb 0feh,06h,7eh,0c0h,0c0h,0c6h,7ch,00h
+	defb 78h,0ch,06h,7eh,0c6h,0c6h,7ch,00h
+	defb 0feh,0c6h,60h,30h,18h,18h,18h,00h
+	defb 7ch,0c6h,0c6h,7ch,0c6h,0c6h,7ch,00h
+	defb 7ch,0c6h,0c6h,0fch,0c0h,0c6h,7ch,00h
+	defb 38h,6ch,0c6h,0c6h,0feh,0c6h,0c6h,00h
+	defb 7eh,0c6h,0c6h,7eh,0c6h,0c6h,7eh,00h
+	defb 78h,0cch,06h,06h,06h,0cch,78h,00h
+	defb 7eh,06h,06h,7eh,06h,06h,7eh,00h
+	defb 7eh,06h,06h,7eh,06h,06h,06h,00h
+	defb 78h,0cch,06h,0f6h,0c6h,0cch,0f8h,00h
+	defb 0c6h,0c6h,0c6h,0feh,0c6h,0c6h,0c6h,00h
+	defb 7eh,18h,18h,18h,18h,18h,7eh,00h
+	defb 0c6h,66h,36h,1eh,36h,66h,0c6h,00h
+	defb 7eh,0d6h,0d6h,0d6h,0d6h,0d6h,0d6h,00h
+	defb 0c6h,0ceh,0deh,0feh,0f6h,0e6h,0c6h,00h
+	defb 7ch,0c6h,0c6h,0c6h,0c6h,0c6h,7ch,00h
+	defb 7eh,0c6h,0c6h,0c6h,7eh,06h,06h,00h
+	defb 7eh,0c6h,0c6h,0e6h,3eh,76h,0e6h,00h
+	defb 7ch,0c6h,06h,7ch,0c0h,0c6h,7ch,00h
+	defb 7eh,18h,18h,18h,18h,18h,18h,00h
+	defb 0c6h,0c6h,0c6h,0c6h,0c6h,0c6h,7ch,00h
+	defb 0c6h,0c6h,0c6h,6ch,6ch,38h,38h,00h
+	defb 66h,66h,66h,3ch,18h,18h,18h,00h
+;	logo
+	defb	0fh, 0fh, 0fh, 0fh, 00h, 00h, 00h, 00h
+	defb	00h, 00h, 00h, 00h, 0fh, 0fh, 0fh, 0fh
+	defb	0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh
+	defb	0f0h, 0f0h, 0f0h, 0f0h, 00h, 00h, 00h, 00h
+	defb	0ffh, 0ffh, 0ffh, 0ffh, 00h, 00h, 00h, 00h
+	defb	0f0h, 0f0h, 0f0h, 0f0h, 0fh, 0fh, 0fh, 0fh
+	defb	0ffh, 0ffh, 0ffh, 0ffh, 0fh, 0fh, 0fh, 0fh
+	defb	00h, 00h, 00h, 00h, 0f0h, 0f0h, 0f0h, 0f0h
+	defb	0fh, 0fh, 0fh, 0fh, 0f0h, 0f0h, 0f0h, 0f0h
+	defb	00h, 00h, 00h, 00h, 0ffh, 0ffh, 0ffh, 0ffh
+	defb	0fh, 0fh, 0fh, 0fh, 0ffh, 0ffh, 0ffh, 0ffh
+	defb	0f0h, 0f0h, 0f0h, 0f0h, 0f0h, 0f0h, 0f0h, 0f0h
+	defb	0ffh, 0ffh, 0ffh, 0ffh, 0f0h, 0f0h, 0f0h, 0f0h
+	defb	0f0h, 0f0h, 0f0h, 0f0h, 0ffh, 0ffh, 0ffh, 0ffh
+	defb	0ffh, 0ffh, 0ffh, 0ffh, 0ffh, 0ffh, 0ffh, 0ffh
+;	8dot
+	defb	06h, 0feh, 0feh, 06h, 06h, 0feh, 0feh, 06h
+	defb	60h, 7fh, 7fh, 60h, 60h, 7fh, 7fh, 60h
+	defb	0f7h, 0f7h, 0f7h, 00h, 7fh, 7fh, 7fh, 00h
+;	16dot
+	defb	00h, 0e0h, 0f0h, 50h, 50h, 0f0h, 0e0h, 00h
+	defb	00h, 01h, 03h, 03h, 03h, 03h, 01h, 00h
+	defb	0f8h, 0ech, 0ech, 20h, 30h, 30h, 30h, 00h
+	defb	07h, 0dh, 0dh, 01h, 03h, 03h, 03h, 00h
+	defb	00h, 10h, 88h, 88h, 88h, 00h, 0fch, 0feh
+	defb	00h, 11h, 08h, 08h, 08h, 00h, 1fh, 3fh
+	defb	0d6h, 0eeh, 0d6h, 0fch, 0fch, 0f8h, 0e0h, 00h
+	defb	35h, 3bh, 35h, 1fh, 1fh, 0fh, 03h, 00h
+	defb	00h, 00h, 00h, 80h, 80h, 80h, 0feh, 0fch
+	defb	00h, 01h, 01h, 03h, 03h, 03h, 0ffh, 7fh
+	defb	0f0h, 0e0h, 0e0h, 0e0h, 0f0h, 30h, 10h, 00h
+	defb	1fh, 0fh, 0fh, 0fh, 1eh, 18h, 10h, 00h
