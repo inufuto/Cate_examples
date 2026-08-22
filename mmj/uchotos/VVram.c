ddb6cd01592ee5d516e0f0e6ce5ada57ca6f3f04
Inufuto <mxb03523@nifty.com>
2026-08-22 19:15:08 +0900

    Add Microcomputer Mahjong UCHOTOS

---
+++ mmj/uchotos/VVram.c
@@ -0,0 +1,38 @@
+#include "VVram.h"
+#include "Sprite.h"
+#include "Vram.h"
+#include "Sound.h"
+
+byte[VVramWidth * VVramHeight] VVram;
+
+void DrawAll()
+{
+    EraseSprites();
+    DrawSprites();
+    VVramToVram();
+    CallSound();
+}
+
+void VPut2CXY(byte x, byte y, byte c)
+{
+    ptr<byte> pVVram = VVramPtr(x, y);
+    repeat (2) {
+        repeat (2) {
+            pVVram = VPut(pVVram, c);
+            ++c;
+        }
+        pVVram += VVramWidth - 2;
+    }
+}
+
+void VPut2SXY(byte x, byte y, ptr<byte> pChars)
+{
+    ptr<byte> pVVram = VVramPtr(x, y);
+    repeat (2) {
+        repeat (2) {
+            pVVram = VPut(pVVram, *pChars);
+            ++pChars;
+        }
+        pVVram += VVramWidth - 2;
+    }
+}
